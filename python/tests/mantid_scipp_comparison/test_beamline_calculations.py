163776b1badcf537d0282553c105879974c2fabb
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-02-26 15:24:50 +0000

    More beamline tests

--- python/tests/mantid_scipp_comparison/test_beamline_calculations.py
+++ python/tests/mantid_scipp_comparison/test_beamline_calculations.py
@@ -1,7 +1,6 @@
 from .mantid_scipp_comparison import MantidScippComparison
 from ..mantid_data_helper import mantid_is_available
 import pytest
-import scippneutron.mantid as converter
 import scippneutron as sn
 import scipp as sc
 
@@ -13,23 +12,55 @@ class BeamlineCalculationsTest(MantidScippComparison):
     @property
     def _workspaces(self):
         import mantid.simpleapi as sapi
-        import mantid.kernel as kernel
         ws = sapi.CreateSampleWorkspace(SourceDistanceFromSample=10.0,
                                         BankDistanceFromSample=1.1,
+                                        BankPixelWidth=1,
                                         NumBanks=1,
+                                        XMax=200,
                                         StoreInADS=False)
         return {"sample_workspace": ws}
 
+
+class L1CalculationsTest(BeamlineCalculationsTest):
     def _run_mantid(self, input):
-        import mantid.simpleapi as sapi
         return input.detectorInfo().l1() * sc.Unit('m')
 
     def _run_scipp(self, input):
         return sn.l1(input)
 
 
+class L2CalculationsTest(BeamlineCalculationsTest):
+    def _run_mantid(self, input):
+        return input.detectorInfo().l2(0) * sc.Unit('m')
+
+    def _run_scipp(self, input):
+        return sn.l2(input)
+
+
+class TwoThetaCalculationsTest(BeamlineCalculationsTest):
+    def _run_mantid(self, input):
+        return input.detectorInfo().twoTheta(0) * sc.Unit('rad')
+
+    def _run_scipp(self, input):
+        return sn.scattering_angle(input)
+
+
 @pytest.mark.skipif(not mantid_is_available(),
                     reason='Mantid framework is unavailable')
 def test_bealine_calculations_l1():
-    test = BeamlineCalculationsTest()
+    test = L1CalculationsTest()
+    print(test.run(allow_failure=True))
+
+
+@pytest.mark.skipif(not mantid_is_available(),
+                    reason='Mantid framework is unavailable')
+def test_bealine_calculations_l2():
+    test = L2CalculationsTest()
+    print(test.run(allow_failure=True))
+
+
+@pytest.mark.skipif(not mantid_is_available(),
+                    reason='Mantid framework is unavailable')
+def test_bealine_calculations_two_theta():
+    test = TwoThetaCalculationsTest()
     print(test.run(allow_failure=True))
