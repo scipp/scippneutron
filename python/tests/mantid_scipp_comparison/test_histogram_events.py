b5e11a7b224c3789fd08dc70f1d866a01b7b9283
Owen Arnold <owen.arnold@stfc.ac.uk>
2021-03-01 11:33:47 +0000

    use numpy linspace

--- python/tests/mantid_scipp_comparison/test_histogram_events.py
+++ python/tests/mantid_scipp_comparison/test_histogram_events.py
@@ -28,7 +28,9 @@ class HistogramEventsTest(MantidScippComparison):
     def _run_scipp(self, input):
         return sc.histogram(x=input,
                             bins=sc.Variable(dims=['tof'],
-                                             values=np.arange(0, 1010, 10),
+                                             values=np.linspace(0,
+                                                                1000,
+                                                                num=101),
                                              dtype=sc.dtype.float64,
                                              unit=sc.units.us))
 
