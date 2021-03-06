1317992b9f9ec96bf13d1020ea28d9a289e98bef
Matthew D Jones <matthew.d.jones@tessella.com>
2021-03-06 10:23:19 +0000

    Test skip sample position using distance if no unit

--- python/tests/test_load_nexus.py
+++ python/tests/test_load_nexus.py
@@ -578,7 +578,6 @@ def test_skips_loading_sample_if_more_than_one_sample_in_file():
     builder.add_sample(Sample("sample_2"))
     with builder.file() as nexus_file:
         loaded_data = scippneutron.load_nexus(nexus_file)
-
     assert loaded_data is None
 
 
@@ -589,10 +588,23 @@ def test_loads_sample_position_from_distance_dataset():
     # can define position and orientation in 3D.
     builder = InMemoryNexusFileBuilder()
     distance = 4.2
-    builder.add_sample(Sample("sample", distance=distance, distance_units="m"))
+    units = "m"
+    builder.add_sample(
+        Sample("sample", distance=distance, distance_units=units))
     with builder.file() as nexus_file:
         loaded_data = scippneutron.load_nexus(nexus_file)
 
     expected_position = np.array([0, 0, distance])
     assert np.allclose(loaded_data["sample_position"].values,
                        expected_position)
+    assert loaded_data["sample_position"].unit == sc.Unit(units)
+
+
+def test_skips_sample_position_from_distance_dataset_missing_unit():
+    builder = InMemoryNexusFileBuilder()
+    distance = 4.2
+    builder.add_sample(Sample("sample", distance=distance,
+                              distance_units=None))
+    with builder.file() as nexus_file:
+        loaded_data = scippneutron.load_nexus(nexus_file)
+    assert loaded_data is None
