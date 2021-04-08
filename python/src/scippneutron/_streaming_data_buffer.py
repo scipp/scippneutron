e8b00ef6bf7178bf13522ddf3e4cf5099442facf
Matthew D Jones <matthew.d.jones@tessella.com>
2021-04-08 16:54:08 +0100

    Remove try-except statements mistakenly left from debugging

--- python/src/scippneutron/_streaming_data_buffer.py
+++ python/src/scippneutron/_streaming_data_buffer.py
@@ -54,20 +54,17 @@ class StreamedDataBuffer:
             self._periodic_emit.cancel()
 
     async def _emit_data(self):
-        try:
-            async with self._buffer_mutex:
-                if self._unrecognised_fb_id_count:
-                    warn(f"Received {self._unrecognised_fb_id_count}"
-                         " messages with unrecognised FlatBuffer ids")
-                    self._unrecognised_fb_id_count = 0
-                if self._current_event == 0:
-                    return
-                new_data = self._events_buffer[
-                    'event', :self._current_event].copy()
-                self._current_event = 0
-            self._emit_queue.put_nowait(new_data)
-        except Exception as e:
-            print(e)
+        async with self._buffer_mutex:
+            if self._unrecognised_fb_id_count:
+                warn(f"Received {self._unrecognised_fb_id_count}"
+                     " messages with unrecognised FlatBuffer ids")
+                self._unrecognised_fb_id_count = 0
+            if self._current_event == 0:
+                return
+            new_data = self._events_buffer[
+                'event', :self._current_event].copy()
+            self._current_event = 0
+        self._emit_queue.put_nowait(new_data)
 
     async def _emit_loop(self):
         while not self._cancelled:
@@ -75,34 +72,32 @@ class StreamedDataBuffer:
             await self._emit_data()
 
     async def new_data(self, new_data: bytes):
+        # Are they event data?
         try:
-            # Are they event data?
-            try:
-                deserialised_data = deserialise_ev42(new_data)
-                message_size = deserialised_data.detector_id.size
-                if message_size > self._buffer_size:
-                    warn("Single message would overflow NewDataBuffer, "
-                         "please restart with a larger buffer_size:\n"
-                         f"message_size: {message_size}, buffer_size:"
-                         f" {self._buffer_size}")
-                # If new data would overfill buffer then emit data
-                # currently in buffer first
-                if self._current_event + message_size > self._buffer_size:
-                    await self._emit_data()
-                async with self._buffer_mutex:
-                    frame = self._events_buffer[
-                        'event',
-                        self._current_event:self._current_event + message_size]
-                    frame.coords[
-                        'detector_id'].values = deserialised_data.detector_id
-                    frame.coords[
-                        'tof'].values = deserialised_data.time_of_flight
-                    frame.coords['pulse_time'].values = \
-                        deserialised_data.pulse_time * \
-                        np.ones_like(deserialised_data.time_of_flight)
-                    self._current_event += message_size
+            deserialised_data = deserialise_ev42(new_data)
+            message_size = deserialised_data.detector_id.size
+            if message_size > self._buffer_size:
+                warn("Single message would overflow NewDataBuffer, "
+                     "please restart with a larger buffer_size:\n"
+                     f"message_size: {message_size}, buffer_size:"
+                     f" {self._buffer_size}. These data have been "
+                     f"skipped!")
                 return
-            except WrongSchemaException:
-                self._unrecognised_fb_id_count += 1
-        except Exception as e:
-            print(e)
+            # If new data would overfill buffer then emit data
+            # currently in buffer first
+            if self._current_event + message_size > self._buffer_size:
+                await self._emit_data()
+            async with self._buffer_mutex:
+                frame = self._events_buffer[
+                    'event',
+                    self._current_event:self._current_event + message_size]
+                frame.coords[
+                    'detector_id'].values = deserialised_data.detector_id
+                frame.coords['tof'].values = deserialised_data.time_of_flight
+                frame.coords['pulse_time'].values = \
+                    deserialised_data.pulse_time * \
+                    np.ones_like(deserialised_data.time_of_flight)
+                self._current_event += message_size
+            return
+        except WrongSchemaException:
+            self._unrecognised_fb_id_count += 1
