bd074b48d3028557aafa07b8a6e40ad348fa07eb
Neil Vaytet <neil.vaytet@esss.se>
2021-05-04 14:23:46 +0200

    list the contents of install include target

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,5 +1,7 @@
 if "%INSTALL_PREFIX%" == "" set INSTALL_PREFIX=%cd%\scipp_install & call tools\make_and_install.bat
 
+dir /s /b %INSTALL_PREFIX%\include
+
 move %INSTALL_PREFIX%\scippneutron %CONDA_PREFIX%\lib\
 move %INSTALL_PREFIX%\bin\scippneutron*.dll %CONDA_PREFIX%\bin\
 move %INSTALL_PREFIX%\Lib\scippneutron*.lib %CONDA_PREFIX%\Lib\
