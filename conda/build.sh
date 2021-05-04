4c61c7194968567bc0c97797d49220ba7562eeb8
Neil Vaytet <neil.vaytet@esss.se>
2021-05-04 13:49:52 +0200

    move include files into existing directory

--- conda/build.sh
+++ conda/build.sh
@@ -12,4 +12,4 @@ mv "$INSTALL_PREFIX"/scippneutron "$CONDA_PREFIX"/lib/python*/
 mv "$INSTALL_PREFIX"/lib/libscippneutron* "$CONDA_PREFIX"/lib/
 mv "$INSTALL_PREFIX"/lib/cmake/scippneutron "$CONDA_PREFIX"/lib/cmake/
 mv "$INSTALL_PREFIX"/include/scippneutron* "$CONDA_PREFIX"/include/
-mv "$INSTALL_PREFIX"/include/scipp "$CONDA_PREFIX"/include/
+mv "$INSTALL_PREFIX"/include/scipp/* "$CONDA_PREFIX"/include/scipp/
