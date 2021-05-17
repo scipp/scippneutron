a7435e1c7a8424ca41f781d445cb6a1a21eb3e4c
Neil Vaytet <neil.vaytet@esss.se>
2021-05-17 10:15:57 +0200

    be careful with wildcard and move on windows

--- conda/bld.bat
+++ conda/bld.bat
@@ -4,5 +4,6 @@ move %INSTALL_PREFIX%\scippneutron %CONDA_PREFIX%\lib\
 move %INSTALL_PREFIX%\bin\scippneutron*.dll %CONDA_PREFIX%\bin\
 move %INSTALL_PREFIX%\Lib\scippneutron*.lib %CONDA_PREFIX%\Lib\
 move %INSTALL_PREFIX%\Lib\cmake\scippneutron %CONDA_PREFIX%\Lib\cmake\
-move %INSTALL_PREFIX%\include\scippneutron* %CONDA_PREFIX%\include\
+move %INSTALL_PREFIX%\include\scippneutron %CONDA_PREFIX%\include\
+move %INSTALL_PREFIX%\include\scippneutron-* %CONDA_PREFIX%\include\
 move %INSTALL_PREFIX%\include\scipp\neutron %CONDA_PREFIX%\include\scipp\
