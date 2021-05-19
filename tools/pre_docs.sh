02297dc5879c353dbdf00aab9c364e30c63d18b1
Neil Vaytet <neil.vaytet@esss.se>
2021-05-19 16:28:44 +0200

    avoid cd by renaming downloaded directory

--- tools/pre_docs.sh
+++ tools/pre_docs.sh
@@ -3,9 +3,8 @@
 set -ex
 
 # Fetch data files from local dmsc web server
-mkdir -p  data
-cd data
-wget -nv -r -np -nH --cut-dirs=3 -R index.html* https://public.esss.dk/users/neil.vaytet/scippneutron
+wget -nv -r -np -nH --cut-dirs=2 -R index.html* https://public.esss.dk/users/neil.vaytet/scippneutron
+mv scippneutron data;
 # Create Mantid properties file so that we can give it the location of the data files
 mkdir -p  $HOME/.mantid
 echo -e "usagereports.enabled=0\ndatasearch.directories=$(pwd)/data" > $HOME/.mantid/Mantid.user.properties
