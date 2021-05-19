2aada8155c0c31fc6280b0cb92c2fa91889a017d
Neil Vaytet <neil.vaytet@esss.se>
2021-05-19 15:53:24 +0200

    test the dmsc web file server

--- tools/pre_docs.sh
+++ tools/pre_docs.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
-python data/fetch_neutron_data.py
+# Fetch data files from local dmsc web server
+mkdir -p  data
+cd data
+wget -nv -r -np -nH --cut-dirs=3 -R index.html* https://public.esss.dk/users/neil.vaytet/scippneutron
+# Create Mantid properties file so that we can give it the location of the data files
 mkdir -p  $HOME/.mantid
 echo -e "usagereports.enabled=0\ndatasearch.directories=$(pwd)/data" > $HOME/.mantid/Mantid.user.properties
