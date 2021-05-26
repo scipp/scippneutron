1a26538210e1c045459efa5a47836d50870f1066
Neil Vaytet <neil.vaytet@esss.se>
2021-05-26 09:40:59 +0200

    change from neil.vaytet user space to groups/scipp directory

--- tools/pre_docs.sh
+++ tools/pre_docs.sh
@@ -5,7 +5,7 @@ set -ex
 # Fetch data files from local dmsc web server.
 # Note that the final '/' is important, if not wget ends up downloading all
 # the other folders on the same level.
-wget -nv -r -np -nH --cut-dirs=2 -R index.html* https://public.esss.dk/users/neil.vaytet/scippneutron/
+wget -nv -r -np -nH --cut-dirs=2 -R index.html* https://public.esss.dk/groups/scipp/scippneutron/
 mv scippneutron data;
 # Create Mantid properties file so that we can give it the location of the data files
 mkdir -p  $HOME/.mantid
