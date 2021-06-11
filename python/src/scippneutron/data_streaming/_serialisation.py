3245882416cc4c0f907d50586f1c40939f37658a
Matthew D Jones <matthew.d.jones@tessella.com>
2021-06-11 15:34:51 +0100

    Workaround deserialising scalar strings

--- python/src/scippneutron/data_streaming/_serialisation.py
+++ python/src/scippneutron/data_streaming/_serialisation.py
@@ -31,14 +31,12 @@ def dict_loads(data_dict: Dict) -> sc.DataArray:
         jfile.write(str(data_dict))
 
     def convert_from_str_unit_and_dtype(d):
+        delete_dtype = False
         for k, v in d.items():
             if isinstance(v, dict):
                 convert_from_str_unit_and_dtype(v)
-                try:
-                    if str(v["dtype"]) in ("DataArray", "DataSet", "Variable"):
-                        d[k] = sc.from_dict(v["value"])
-                except KeyError:
-                    pass
+                if k not in ("attrs", "masks", "coords"):
+                    d[k] = sc.from_dict(v)
             else:
                 if k == "unit":
                     d[k] = sc.Unit(v)
@@ -46,7 +44,36 @@ def dict_loads(data_dict: Dict) -> sc.DataArray:
                     try:
                         d[k] = np.dtype(v)
                     except TypeError:
-                        pass  # leave DataArray etc alone
+                        if v == "string":
+                            d[k] = sc.dtype.string
+                            try:
+                                # Workaround for not being able to construct a
+                                # variable from a scalar string numpy array.
+                                # See
+                                # https://github.com/scipp/scipp/issues/1974
+                                d["value"] = d["value"].item()
+                                delete_dtype = True
+                            except AttributeError:
+                                d[k] = sc.dtype.string
+                        # elif any(scipp_container_type in k
+                        #          for scipp_container_type in ("DataArray",
+                        #                                       "DataSet",
+                        #                                       "Variable")):
+                        #     delete_dtype = True
+        if delete_dtype:
+            del d["dtype"]
 
     convert_from_str_unit_and_dtype(data_dict)
-    return sc.from_dict(data_dict)
+    return data_dict
+
+
+if __name__ == "__main__":
+    test_var = sc.Variable(value="commissioning", unit=sc.units.dimensionless)
+    test = sc.to_dict(test_var)
+
+    from scippneutron.file_loading.load_nexus import load_nexus
+    amor_data = load_nexus("/home/matt/git/generate-nexus-files/examples"
+                           "/amor/amor2020n000346_tweaked.nxs")
+    amor_ser = dict_dumps(amor_data)
+    out_amor = dict_loads(amor_ser)
+    print(out_amor)
