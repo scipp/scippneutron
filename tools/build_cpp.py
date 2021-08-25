ff2ec2ad905def42b88182e8d4b100bd1b210d92
Neil Vaytet <neil.vaytet@esss.se>
2021-08-25 18:29:41 +0200

    also use sbt for docs conf

--- tools/build_cpp.py
+++ tools/build_cpp.py
@@ -3,17 +3,25 @@
 
 import scippbuildtools as sbt
 
-if __name__ == '__main__':
 
-    args = sbt.cpp_argument_parser().parse_known_args()[0]
-
-    builder = sbt.CppBuilder(prefix=args.prefix,
-                             build_dir=args.build_dir,
-                             source_dir=args.source_dir,
-                             caching=args.caching)
+def main(prefix, build_dir, source_dir, caching):
+    builder = sbt.CppBuilder(prefix=prefix,
+                             build_dir=build_dir,
+                             source_dir=source_dir,
+                             caching=caching)
 
     builder.cmake_configure()
     builder.enter_build_dir()
     builder.cmake_run()
     builder.cmake_build(['all-benchmarks', 'all-tests', 'install'])
     builder.run_cpp_tests(['scippneutron-test'])
+
+
+if __name__ == '__main__':
+
+    args = sbt.cpp_argument_parser().parse_known_args()[0]
+
+    main(prefix=args.prefix,
+         build_dir=args.build_dir,
+         source_dir=args.source_dir,
+         caching=args.caching)
