f14cd650885dfaac8cde9b8f61611fc543be0902
Neil Vaytet <neil.vaytet@esss.se>
2021-08-25 17:00:10 +0200

    just print dir

--- tools/build_cpp.py
+++ tools/build_cpp.py
@@ -5,8 +5,8 @@ import scippbuildtools as sbt
 import os
 
 if __name__ == '__main__':
-    os.write(1, "SBT version: {}\n".format(sbt.__version__).encode())
-    os.write(1, "DIR: {}\n".format(dir(sbt)).encode())
+    # os.write(1, "SBT version: {}\n".format(sbt.__version__).encode())
+    os.write(1, "SBT DIR: {}\n".format(dir(sbt)).encode())
 
     args = sbt.cpp_argument_parser().parse_known_args()[0]
 
