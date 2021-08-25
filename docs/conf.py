9ff02626bf24640f86dce4db74526d31ed1af3a2
Neil Vaytet <neil.vaytet@esss.se>
2021-08-25 18:41:12 +0200

    flake8

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-from scippbuildtools.sphinxconf import *
+from scippbuildtools.sphinxconf import *  # noqa: F403
 
 project = u'scippneutron'
 
-nbsphinx_prolog = nbsphinx_prolog.replace("XXXX", "scippneutron")
+nbsphinx_prolog = nbsphinx_prolog.replace("XXXX", "scippneutron")  # noqa: F405
