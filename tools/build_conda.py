1da1fff2df33cad6667aefb665b4a98f7c098967
Tom Willemsen <tomwillemsen1995@gmail.com>
2021-09-07 16:47:59 +0100

    Use more aggressive wildcards

--- tools/build_conda.py
+++ tools/build_conda.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
 
     m.move(['scippneutron'], [lib_dest])
     if dll_src is not None:
-        m.move([dll_src, 'scippneutron-*'], [dll_dest])
-    m.move([lib_src, '*scippneutron*'], [lib_src])
+        m.move([dll_src, '*'], [dll_dest])
+    m.move([lib_src, '*'], [lib_src])
     m.move([lib_src, 'cmake', 'scippneutron', '*'], [lib_src, 'cmake', 'scippneutron'])
-    m.move([inc_src, 'scippneutron*'], [inc_src])
+    m.move([inc_src, '*'], [inc_src])
