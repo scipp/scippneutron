056b2d15f2c22b73653a20df1d7108d9cbd31def
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2021-09-15 12:10:30 +0200

    Determine inelastic/elastic outside of conversion_graph

--- python/src/scippneutron/conversion.py
+++ python/src/scippneutron/conversion.py
@@ -201,30 +201,15 @@ _SCATTER_GRAPH_DYNAMICS_BY_ORIGIN = {
 }
 
 
-def _find_inelastic_inputs(data):
-    return [name for name in ('incident_energy', 'final_energy') if name in data.coords]
-
-
-def _inelastic_scatter_graph(data):
-    inputs = _find_inelastic_inputs(data)
-    if len(inputs) > 1:
-        raise RuntimeError(
-            "Data contains coords for incident *and* final energy, cannot have "
-            "both for inelastic scattering.")
-    if len(inputs) == 0:
-        raise RuntimeError(
-            "Data contains neither coords for incident nor for final energy, this "
-            "does not appear to be inelastic-scattering data, cannot convert to "
-            "energy transfer.")
-
+def _inelastic_scatter_graph(energy_mode):
     inelastic_step = {
-        'incident_energy': {
+        'direct_inelastic': {
             'energy_transfer': _energy_transfer_direct_from_tof
         },
-        'final_energy': {
+        'indirect_inelastic': {
             'energy_transfer': _energy_transfer_indirect_from_tof
         }
-    }[inputs[0]]
+    }[energy_mode]
     return {**_SCATTER_GRAPH_KINEMATICS, **inelastic_step}
 
 
@@ -233,52 +218,69 @@ def _reachable_by(target, graph):
                for targets in graph.keys())
 
 
-def _elastic_scatter_base_graph(origin, target):
+def _elastic_scatter_graph(origin, target):
     if _reachable_by(target, _SCATTER_GRAPH_KINEMATICS):
         return dict(_SCATTER_GRAPH_KINEMATICS)
     return {**_SCATTER_GRAPH_KINEMATICS, **_SCATTER_GRAPH_DYNAMICS_BY_ORIGIN[origin]}
 
 
-def _elastic_scatter_graph(data, origin, target):
-    if target == 'energy':
-        inelastic_inputs = _find_inelastic_inputs(data)
-        if inelastic_inputs:
-            raise RuntimeError(
-                f"Data contains coords for inelastic scattering "
-                f"({inelastic_inputs}) but conversion to elastic energy requested. "
-                f"This is not implemented.")
-    return _elastic_scatter_base_graph(origin, target)
-
-
-def _scatter_graph(data, origin, target):
-    graph = (_inelastic_scatter_graph(data) if target == 'energy_transfer' else
-             _elastic_scatter_graph(data, origin, target))
-    if not graph:
-        raise RuntimeError(f"No viable conversion from '{origin}' to '{target}'.")
+def _scatter_graph(origin, target, energy_mode):
+    graph = (_elastic_scatter_graph(origin, target)
+             if energy_mode == 'elastic' else _inelastic_scatter_graph(energy_mode))
     return graph
 
 
-def conversion_graph(data: Union[sc.DataArray, sc.Dataset], origin: str, target: str,
-                     scatter: bool) -> Dict[Union[str, Tuple[str]], Callable]:
+def conversion_graph(origin: str, target: str, scatter: bool,
+                     energy_mode: str) -> Dict[Union[str, Tuple[str]], Callable]:
     """
-    Get the conversion graph used by :py:func:`scippneutron.convert`
-    when called with identical arguments.
+    Get a conversion graph for given parameters.
 
     The graph can be used with `scipp.transform_coords`.
 
-    :param data: Input data to a conversion.
     :param origin: Name of the input coordinate.
     :param target: Name of the output coordinate.
     :param scatter: Choose whether to use scattering or non-scattering conversions.
+    :param energy_mode: Select if energy is conserved. One of `'elastic'`,
+                        `'direct_inelastic'`, `'indirect_inelastic'`.
     :return: Conversion graph.
     :seealso: :py:func:`scippneutron.convert`.
     """
 
     # Results are copied to ensure users do not modify the global dictionaries.
     if scatter:
-        return dict(_scatter_graph(data, origin, target))
+        return dict(_scatter_graph(origin, target, energy_mode))
     else:
         return dict(_NO_SCATTER_GRAPH)
+
+
+def _find_inelastic_inputs(data):
+    return [name for name in ('incident_energy', 'final_energy') if name in data.coords]
+
+
+def _deduce_energy_mode(data, origin, target):
+    inelastic_inputs = _find_inelastic_inputs(data)
+    if target == 'energy_transfer':
+        if len(inelastic_inputs) > 1:
+            raise RuntimeError(
+                "Data contains coords for incident *and* final energy, cannot have "
+                "both for inelastic scattering.")
+        if len(inelastic_inputs) == 0:
+            raise RuntimeError(
+                "Data contains neither coords for incident nor for final energy, this "
+                "does not appear to be inelastic-scattering data, cannot convert to "
+                "energy transfer.")
+        return {
+            'incident_energy': 'direct_inelastic',
+            'final_energy': 'indirect_inelastic'
+        }[inelastic_inputs[0]]
+
+    if 'energy' in (origin, target):
+        if inelastic_inputs:
+            raise RuntimeError(
+                f"Data contains coords for inelastic scattering "
+                f"({inelastic_inputs}) but conversion with elastic energy requested. "
+                f"This is not implemented.")
+    return 'elastic'
 
 
 def convert(data: Union[sc.DataArray, sc.Dataset], origin: str, target: str,
@@ -298,10 +300,12 @@ def convert(data: Union[sc.DataArray, sc.Dataset], origin: str, target: str,
               the possible conversions.
     """
 
+    energy_mode = _deduce_energy_mode(data, origin, target)
+
     try:
         converted = data.transform_coords(target,
                                           graph=conversion_graph(
-                                              data, origin, target, scatter))
+                                              origin, target, scatter, energy_mode))
     except KeyError as err:
         if err.args[0] == target:
             raise RuntimeError(f"No viable conversion from '{origin}' to '{target}' "
