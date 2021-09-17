a1aed530a7e9bfefe8a034e636aac1d0ba858850
Simon Heybrock <simon.heybrock@esss.se>
2021-09-17 15:47:06 +0200

    Fix paths

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cd $PREFIX/cmake-package-test
+cd cmake-package-test
 mkdir -p build && cd build
 cmake -DPKG_VERSION=$PKG_VERSION -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
+cd ../..
 
-cd $PREFIX/docs
+cd docs
 docs_build_dir=$(mktemp -d)
 python build_docs.py --builder=html --prefix=$docs_build_dir --work_dir=$(mktemp -d)
 python build_docs.py --builder=doctest --prefix=$docs_build_dir --work_dir=$(mktemp -d) --no-setup
+cd ..
