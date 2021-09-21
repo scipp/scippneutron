fdb8fcf21e238dc5cf220ef45ce186119819d976
Simon Heybrock <simon.heybrock@esss.se>
2021-09-21 15:34:07 +0200

    Improve package test to also exercise linking to scippneutron

--- conda/cmake-package-test/main.cpp
+++ conda/cmake-package-test/main.cpp
@@ -1,7 +1,14 @@
-#include <scipp/dataset/dataset.h>
+#include <scipp/dataset/data_array.h>
 #include <scipp/neutron/convert.h>
 
 int main() {
-  scipp::Dataset d;
-  d + d;
+  scipp::DataArray da(
+      scipp::variable::makeVariable<double>(scipp::variable::Values{1.0}));
+  da + da;
+  try {
+    scipp::neutron::convert(da, scipp::neutron::NeutronDim::Tof,
+                            scipp::neutron::NeutronDim::Wavelength,
+                            scipp::neutron::ConvertMode::Scatter);
+  } catch (scipp::except::NotFoundError &) {
+  }
 }
