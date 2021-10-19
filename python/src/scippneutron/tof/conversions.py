707a1a8d882d26b9d775eb71694053034292fdf5
Simon Heybrock <simon.heybrock@esss.se>
2021-10-19 14:31:22 +0200

    Docstrings

--- python/src/scippneutron/tof/conversions.py
+++ python/src/scippneutron/tof/conversions.py
@@ -1,6 +1,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 # @author Simon Heybrock
+r"""
+Coordinate transformation graphs for time-of-flight neutron scattering data.
+
+Each graph is a ``dict`` and intended for use with ``sc.transform_coords``.
+Typically multiple graphs need to be combined for a full transformation, e.g., a
+"beamline" graph with an "elastic" graph.
+"""
+
 from ..core.conversion import _SCATTER_GRAPH_KINEMATICS, _NO_SCATTER_GRAPH_KINEMATICS, \
         _SCATTER_GRAPH_DYNAMICS_BY_ORIGIN, _energy_transfer_direct_from_tof, \
         _energy_transfer_indirect_from_tof, _wavelength_from_tof, \
@@ -8,10 +16,22 @@ from ..core.conversion import _SCATTER_GRAPH_KINEMATICS, _NO_SCATTER_GRAPH_KINEM
 
 
 def incident_beam():
+    """
+    Graph for computing 'incident_beam'.
+    """
     return {'incident_beam': _incident_beam}
 
 
 def beamline(scatter: bool):
+    """
+    Graph defining a simple beamline geometry.
+
+    This can be used as part of transformation graphs that require, e.g., scattering
+    angles (``two_theta``) or flight path lengths (``L1`` and ``L2``).
+
+    :param scatter: If True, a graph for scattering from ``sample_position`` is
+                    returned, else a graph without scattering.
+    """
     if scatter:
         return dict(_SCATTER_GRAPH_KINEMATICS)
     else:
@@ -20,18 +40,35 @@ def beamline(scatter: bool):
 
 def kinematic(start: str):
     """
-    Returns a purely kinematic graph with no scattering.
+    Purely kinematic graph with no scattering.
+
+    :param start: Input coordinate. Currently only 'tof' is supported.
     """
     return {'wavelength': _wavelength_from_tof, 'energy': _energy_from_tof}
 
 
 def elastic(start: str):
+    """
+    Graph for elastic scattering transformations.
+
+    :param start: Input coordinate. One of 'energy', 'tof', 'Q', or 'wavelength'.
+    """
     return dict(_SCATTER_GRAPH_DYNAMICS_BY_ORIGIN[start])
 
 
 def direct_inelastic(start: str):
+    """
+    Graph for direct-inelastic scattering transformations.
+
+    :param start: Input coordinate. Currently only 'tof' is supported.
+    """
     return {'tof': {'energy_transfer': _energy_transfer_direct_from_tof}}[start]
 
 
 def indirect_inelastic(start: str):
+    """
+    Graph for indirect-inelastic scattering transformations.
+
+    :param start: Input coordinate. Currently only 'tof' is supported.
+    """
     return {'tof': {'energy_transfer': _energy_transfer_indirect_from_tof}}[start]
