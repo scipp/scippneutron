a029d5be3c6649ef3cae673c6fd487b5e7064468
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-10-20 11:26:09 +0100

    Rename load_nexus to load_event_nexus in the benchmarks

--- benchmarks/load_nexus.py
+++ benchmarks/load_nexus.py
@@ -5,5 +5,5 @@ class LoadNexus:
     def setup(self):
         self.file_path = scn.data.get_path('PG3_4844_event.nxs')
 
-    def time_load_nexus(self):
+    def time_load_event_nexus(self):
         scn.load_nexus(self.file_path)
