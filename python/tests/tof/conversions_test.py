3047bc402ab293b7d35a840d1a19f07977ecccbd
Simon Heybrock <simon.heybrock@esss.se>
2021-10-20 13:12:10 +0200

    Improve tests

--- python/tests/tof/conversions_test.py
+++ python/tests/tof/conversions_test.py
@@ -6,51 +6,40 @@ from scippneutron.tof import conversions
 
 
 def test_incident_beam():
-    graph = conversions.incident_beam()
-    assert 'incident_beam' in graph
-    assert len(graph) == 1
+    assert set(conversions.incident_beam().keys()) == set(['incident_beam'])
 
 
 def test_scattered_beam():
-    graph = conversions.scattered_beam()
-    assert 'scattered_beam' in graph
-    assert len(graph) == 1
+    assert set(conversions.scattered_beam().keys()) == set(['scattered_beam'])
 
 
 def test_two_theta():
-    graph = conversions.two_theta()
-    assert 'two_theta' in graph
-    assert len(graph) == 3
+    assert set(conversions.two_theta().keys()) == set(
+        ['scattered_beam', 'incident_beam', 'two_theta'])
 
 
 def test_L1():
-    graph = conversions.L1()
-    assert 'L1' in graph
-    assert len(graph) == 2
+    assert set(conversions.L1().keys()) == set(['L1', 'incident_beam'])
 
 
 def test_L2():
-    graph = conversions.L2()
-    assert 'L2' in graph
-    assert len(graph) == 2
+    assert set(conversions.L2().keys()) == set(['L2', 'scattered_beam'])
 
 
 def test_Ltotal():
-    graph = conversions.Ltotal(scatter=False)
-    assert 'Ltotal' in graph
-    assert len(graph) == 1
-    graph = conversions.Ltotal(scatter=True)
-    assert 'Ltotal' in graph
-    assert len(graph) == 5
+    assert set(conversions.Ltotal(scatter=False).keys()) == set(['Ltotal'])
+    assert set(conversions.Ltotal(scatter=True).keys()) == set(
+        ['scattered_beam', 'incident_beam', 'L1', 'L2', 'Ltotal'])
 
 
 def test_beamline():
-    assert len(conversions.beamline(scatter=False)) == 1
-    assert len(conversions.beamline(scatter=True)) == 6
+    assert set(conversions.beamline(scatter=False).keys()) == set(['Ltotal'])
+    assert set(conversions.beamline(scatter=True).keys()) == set(
+        ['scattered_beam', 'incident_beam', 'L1', 'L2', 'Ltotal', 'two_theta'])
 
 
 def test_kinematic():
-    assert len(conversions.kinematic('tof')) == 2
+    assert set(conversions.kinematic('tof').keys()) == set(['energy', 'wavelength'])
     # Other initial coords not supported for now
     with pytest.raises(KeyError):
         conversions.kinematic('energy')
@@ -59,63 +48,45 @@ def test_kinematic():
 
 
 def test_elastic():
-    assert len(conversions.elastic('energy')) == 2
-    assert len(conversions.elastic('tof')) == 4
-    assert len(conversions.elastic('Q')) == 1
-    assert len(conversions.elastic('wavelength')) == 3
+    assert set(conversions.elastic('energy').keys()) == set(['dspacing', 'wavelength'])
+    assert set(conversions.elastic('tof').keys()) == set(
+        ['dspacing', 'energy', 'Q', 'wavelength'])
+    assert set(conversions.elastic('Q').keys()) == set(['wavelength'])
+    assert set(conversions.elastic('wavelength').keys()) == set(
+        ['dspacing', 'energy', 'Q'])
 
 
 def test_elastic_dspacing():
-    graph = conversions.elastic_dspacing('energy')
-    assert 'dspacing' in graph
-    assert len(graph) == 1
-    graph = conversions.elastic_dspacing('tof')
-    assert 'dspacing' in graph
-    assert len(graph) == 1
-    graph = conversions.elastic_dspacing('wavelength')
-    assert 'dspacing' in graph
-    assert len(graph) == 1
+    assert set(conversions.elastic_dspacing('energy').keys()) == set(['dspacing'])
+    assert set(conversions.elastic_dspacing('tof').keys()) == set(['dspacing'])
+    assert set(conversions.elastic_dspacing('wavelength').keys()) == set(['dspacing'])
 
 
 def test_elastic_energy():
-    graph = conversions.elastic_energy('tof')
-    assert 'energy' in graph
-    assert len(graph) == 1
-    graph = conversions.elastic_energy('wavelength')
-    assert 'energy' in graph
-    assert len(graph) == 1
+    assert set(conversions.elastic_energy('tof').keys()) == set(['energy'])
+    assert set(conversions.elastic_energy('wavelength').keys()) == set(['energy'])
 
 
 def test_elastic_Q():
-    graph = conversions.elastic_Q('tof')
-    assert 'Q' in graph
-    assert len(graph) == 2
-    graph = conversions.elastic_Q('wavelength')
-    assert 'Q' in graph
-    assert len(graph) == 1
+    assert set(conversions.elastic_Q('tof').keys()) == set(['Q', 'wavelength'])
+    assert set(conversions.elastic_Q('wavelength').keys()) == set(['Q'])
 
 
 def test_elastic_wavelength():
-    graph = conversions.elastic_wavelength('energy')
-    assert 'wavelength' in graph
-    assert len(graph) == 1
-    graph = conversions.elastic_wavelength('tof')
-    assert 'wavelength' in graph
-    assert len(graph) == 1
-    graph = conversions.elastic_wavelength('Q')
-    assert 'wavelength' in graph
-    assert len(graph) == 1
+    assert set(conversions.elastic_wavelength('energy').keys()) == set(['wavelength'])
+    assert set(conversions.elastic_wavelength('tof').keys()) == set(['wavelength'])
+    assert set(conversions.elastic_wavelength('Q').keys()) == set(['wavelength'])
 
 
 def test_direct_inelastic():
-    assert len(conversions.direct_inelastic('tof')) == 1
+    assert set(conversions.direct_inelastic('tof').keys()) == set(['energy_transfer'])
     # Other initial coords not supported for now
     with pytest.raises(KeyError):
         conversions.kinematic('wavelength')
 
 
 def test_indirect_inelastic():
-    assert len(conversions.indirect_inelastic('tof')) == 1
+    assert set(conversions.indirect_inelastic('tof').keys()) == set(['energy_transfer'])
     # Other initial coords not supported for now
     with pytest.raises(KeyError):
         conversions.kinematic('wavelength')
