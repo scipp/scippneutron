b668ab8577b06b34554da63cbc17999c0c636709
Simon Heybrock <simon.heybrock@esss.se>
2021-10-22 09:35:50 +0200

    Fix dummy project testing scippneutron cmake package

--- conda/cmake-package-test/main.cpp
+++ conda/cmake-package-test/main.cpp
@@ -1,14 +1,12 @@
 #include <scipp/dataset/data_array.h>
-#include <scipp/neutron/convert.h>
+#include <scipp/neutron/beamline.h>
 
 int main() {
   scipp::DataArray da(
       scipp::variable::makeVariable<double>(scipp::variable::Values{1.0}));
   da + da;
   try {
-    scipp::neutron::convert(da, scipp::neutron::NeutronDim::Tof,
-                            scipp::neutron::NeutronDim::Wavelength,
-                            scipp::neutron::ConvertMode::Scatter);
+    scipp::neutron::L1(da.coords());
   } catch (scipp::except::NotFoundError &) {
   }
 }
