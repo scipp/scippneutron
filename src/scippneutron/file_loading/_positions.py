ac77dedf750e8af603b936b4671469410b440d23
Neil Vaytet <neil.vaytet@esss.se>
2022-01-03 14:03:34 +0100

    add file identifiers

--- src/scippneutron/file_loading/_positions.py
+++ src/scippneutron/file_loading/_positions.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
 from _warnings import warn
 from typing import List, Optional, Tuple, Any
 import numpy as np
--- src/scippneutron/file_loading/_sample.py
+++ src/scippneutron/file_loading/_sample.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
 from typing import List, Tuple, Union
 import numpy as np
 import scipp as sc
