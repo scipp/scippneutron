eb4ed0b36d3edcfd0283eb22888c2472c4e8b243
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-11 10:26:44 +0100

    Add intersphinx mappings for python, scipy

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,9 @@ rst_epilog = f"""
 """  # noqa: E501
 
 intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
     'numpy': ('https://docs.scipy.org/doc/numpy/', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
     'xarray': ('https://xarray.pydata.org/en/stable/', None)
 }
 
