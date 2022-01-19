efd6dcf0d9de8c45b3b67e7a8159e818bfd223a1
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-19 14:32:03 +0100

    Remove allow_failure flag

--- tests/mantid_scipp_comparison/mantid_scipp_comparison.py
+++ tests/mantid_scipp_comparison/mantid_scipp_comparison.py
@@ -17,25 +17,17 @@ class MantidScippComparison(ABC):
         return result, (stop - start) * sc.Unit('s')
 
     @staticmethod
-    def _assert(a, b, allow_failure):
-        try:
-            rtol = 1e-9 * sc.units.one
-            atol = 1e-9 * a.unit
-            if isinstance(a, sc.DataArray):
-                assert (sc.allclose(a.data, b.data, rtol=rtol, atol=atol)
-                        and sc.utils.comparison.isnear(
-                            a, b, rtol=1e-6 * sc.units.one, include_data=False))
-            else:
-                assert sc.all(sc.isclose(a, b, rtol=rtol, atol=atol)).value
-        # Catching RuntimeError because isnear raises it when the
-        # number of meta items does not match.
-        except (AssertionError, RuntimeError) as ae:
-            if allow_failure:
-                print(ae)
-            else:
-                raise ae
+    def _assert(a, b):
+        rtol = 1e-9 * sc.units.one
+        atol = 1e-9 * a.unit
+        if isinstance(a, sc.DataArray):
+            assert (sc.allclose(a.data, b.data, rtol=rtol, atol=atol)
+                    and sc.utils.comparison.isnear(
+                        a, b, rtol=1e-6 * sc.units.one, include_data=False))
+        else:
+            assert sc.all(sc.isclose(a, b, rtol=rtol, atol=atol)).value
 
-    def _run_from_workspace(self, in_ws, allow_failure):
+    def _run_from_workspace(self, in_ws):
         out_mantid, time_mantid = self._execute_with_timing(self._run_mantid,
                                                             input=in_ws)
         in_da = mantid.from_mantid(in_ws)
@@ -43,7 +35,7 @@ class MantidScippComparison(ABC):
             in_da = in_da.astype(sc.DType.float64)  # Converters set weights float32
         out_scipp, time_scipp = self._execute_with_timing(self._run_scipp, input=in_da)
 
-        self._assert(out_scipp, out_mantid, allow_failure)
+        self._assert(out_scipp, out_mantid)
 
         return {'scipp': out_scipp, 'mantid': out_mantid}
 
@@ -51,7 +43,7 @@ class MantidScippComparison(ABC):
         results[f'with_{name}' if self._test_description is None else
                 f'{self._test_description}_with_{name}'] = result
 
-    def run(self, allow_failure=False):
+    def run(self):
         import mantid.simpleapi as sapi
         results = {}
         if self._filenames == {} and self._workspaces == {}:
@@ -60,10 +52,10 @@ class MantidScippComparison(ABC):
             file = get_path(filename)
             print('Loading', filename)
             in_ws = sapi.Load(Filename=file, StoreInADS=False)
-            result = self._run_from_workspace(in_ws, allow_failure)
+            result = self._run_from_workspace(in_ws)
             self._append_result(filename, result, results)
         for name, in_ws in self._workspaces.items():
-            result = self._run_from_workspace(in_ws, allow_failure)
+            result = self._run_from_workspace(in_ws)
             self._append_result(name, result, results)
 
         return results
