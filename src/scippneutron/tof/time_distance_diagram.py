7da02bf8822604b1af96347f93e5b929455b4955
Simon Heybrock <simon.heybrock@ess.eu>
2022-01-31 10:49:49 +0100

    Unused code

--- src/scippneutron/tof/time_distance_diagram.py
+++ src/scippneutron/tof/time_distance_diagram.py
@@ -1,32 +1,6 @@
 import scipp as sc
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-
-
-def deg_to_rad(x):
-    return x * np.pi / 180.0
-
-
-class Chopper:
-    def __init__(self,
-                 frequency=0,
-                 openings=None,
-                 distance=0,
-                 phase=0,
-                 unit="rad",
-                 name=""):
-        # openings is list. First entry is start angle of the first cut-out
-        # second entry is end angle of first cut-out, etc.
-        self.frequency = frequency
-        self.openings = openings
-        self.omega = 2.0 * np.pi * frequency
-        self.distance = distance
-        self.phase = phase
-        if unit == "deg":
-            self.openings = deg_to_rad(self.openings)
-            self.phase = deg_to_rad(self.phase)
-        self.name = name
 
 
 class Beamline:
