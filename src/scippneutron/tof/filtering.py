b0d837fc52e9e79bb5d09f6f2b3c3337b32c3bce
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-03-14 16:12:19 +0100

    Draft of remove_bad_pulses

---
+++ src/scippneutron/tof/filtering.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @author Jan-Lukas Wynen
+"""
+Event filtering for time-of-flight source.
+"""
+from contextlib import contextmanager
+import scipp as sc
+
+
+@contextmanager
+def _temporary_bin_coord(data, name, coord):
+    data.bins.coords[name] = coord
+    yield
+    del data.bins.coords[name]
+
+
+def _with_pulse_time_edges(da):
+    pulse_time = da.coords[da.dim]
+    one = sc.scalar(1, dtype='int64', unit=pulse_time.unit)
+    lo = pulse_time[0] - one
+    hi = pulse_time[-1] + one
+    mid = sc.midpoints(pulse_time)
+    da.coords[da.dim] = sc.concat([lo, mid, hi], da.dim)
+    return da
+
+
+def remove_bad_pulses(data, *, proton_charge, threshold_factor):
+    min_charge = proton_charge.data.mean() * threshold_factor
+    good_pulse = _with_pulse_time_edges(proton_charge >= min_charge)
+    with _temporary_bin_coord(
+            data, 'good_pulse',
+            sc.lookup(good_pulse, good_pulse.dim)[data.bins.coords[good_pulse.dim]]):
+        filtered = sc.bin(data, groups=[sc.array(dims=['good_pulse'], values=[True])])
+    return filtered.squeeze('good_pulse')
---
+++ tests/tof/filtering_test.py
@@ -0,0 +1,100 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @author Jan-Lukas Wynen
+import numpy as np
+import scipp as sc
+
+from scippneutron.tof import filtering
+
+# TODO proton charge time monotonic?
+
+# TODO pass charge as string
+
+
+def make_data_with_pulse_time(rng, n_event):
+    start_time = sc.scalar(np.datetime64('2022-03-14T14:42:37.12345', 'ns'))
+    pulse_time = (
+        start_time +
+        sc.array(dims=['event'], values=rng.integers(0, 10**6, n_event), unit='ns'))
+    events = sc.DataArray(sc.array(dims=['event'],
+                                   values=rng.uniform(0.0, 2.0, n_event),
+                                   unit='counts'),
+                          coords={
+                              'tof':
+                              sc.array(dims=['event'],
+                                       values=rng.integers(10, 1000, n_event),
+                                       unit='us'),
+                              'pulse_time':
+                              pulse_time,
+                              'spectrum':
+                              sc.array(dims=['event'],
+                                       values=rng.integers(0, 10, n_event),
+                                       unit=None),
+                          })
+    return sc.bin(events,
+                  edges=[sc.array(dims=['tof'], values=[10, 500, 1000], unit='us')],
+                  groups=[sc.arange('spectrum', 0, 10, unit=None)])
+
+
+def test_make_data_with_pulse_time():
+    rng = np.random.default_rng(9461)
+    data = make_data_with_pulse_time(rng, 100)
+    assert 'pulse_time' in data.bins.coords
+
+
+def make_data_with_pulse_time_and_proton_charge(rng, n_event, n_proton_charge,
+                                                bad_charge, bad_charge_indices):
+    data = make_data_with_pulse_time(rng, n_event)
+
+    start_time = data.bins.coords['pulse_time'].min()
+    pulse_time = (start_time + sc.linspace(
+        'pulse_time', 0, 10**6, n_proton_charge, unit='ns').to(dtype='int64'))
+    good_charge_value = bad_charge.value * 100
+    proton_charge = sc.DataArray(sc.array(dims=['pulse_time'],
+                                          values=rng.uniform(good_charge_value,
+                                                             good_charge_value * 1.2,
+                                                             n_proton_charge),
+                                          unit=bad_charge.unit),
+                                 coords={'pulse_time': pulse_time})
+
+    for i in bad_charge_indices:
+        proton_charge[i] = bad_charge
+
+    data.attrs['proton_charge'] = sc.scalar(proton_charge)
+    return data
+
+
+def test_make_data_with_pulse_time_and_proton_charge():
+    rng = np.random.default_rng(65501)
+    bad_charge = sc.scalar(1.0e5, unit='pC')
+    data = make_data_with_pulse_time_and_proton_charge(rng, 100, 300, bad_charge,
+                                                       [0, 2, 4])
+    assert 'pulse_time' in data.bins.coords
+    assert sc.identical(data.attrs['proton_charge'].value.data[0], bad_charge)
+    assert sc.identical(data.attrs['proton_charge'].value.data[2], bad_charge)
+    assert sc.identical(data.attrs['proton_charge'].value.data[4], bad_charge)
+    assert (data.attrs['proton_charge'].value.data[1] > bad_charge).value
+    assert (data.attrs['proton_charge'].value.data[3] > bad_charge).value
+
+
+def test_remove_bad_pulses_contiguous_section():
+    rng = np.random.default_rng(65501)
+    bad_charge = sc.scalar(1.0e5, unit='pC')
+    bad_indices = np.arange(100, 120)
+    data = make_data_with_pulse_time_and_proton_charge(rng, 100, 300, bad_charge,
+                                                       bad_indices)
+
+    proton_charge = data.attrs['proton_charge'].value
+    begin_removed = proton_charge.coords['pulse_time'][100]
+    end_removed = proton_charge.coords['pulse_time'][120]
+    data.coords['should_be_removed'] = (data.bins.coords['pulse_time'] >
+                                        begin_removed) & (data.bins.coords['pulse_time']
+                                                          < end_removed)
+
+    filtered = filtering.remove_bad_pulses(data,
+                                           proton_charge=proton_charge,
+                                           threshold_factor=0.9)
+
+    assert not sc.any(filtered.coords['should_be_removed']).value
+    assert sc.identical(filtered.bins.size(),
+                        data.bins.size() - data.coords['should_be_removed'].sum())
