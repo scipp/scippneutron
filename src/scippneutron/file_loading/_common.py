115b04aad764f8f22a1a3744dabed9164a07c348
Simon Heybrock <simon.heybrock@ess.eu>
2022-03-14 13:57:42 +0100

    Unused code

--- src/scippneutron/file_loading/_common.py
+++ src/scippneutron/file_loading/_common.py
@@ -2,10 +2,8 @@
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 # @author Matthew Jones
 
-from typing import Union, Optional, Any, Tuple, Dict, List
+from typing import Union, Tuple, Dict, List
 import h5py
-from scipp.spatial import affine_transform, linear_transform, \
-    rotation, translation
 import scipp as sc
 import numpy as np
 
@@ -44,39 +42,6 @@ class JSONGroup(dict):
 
 Dataset = Union[h5py.Dataset, Dict]
 Group = Union[h5py.Group, JSONGroup]
-
-
-def _add_attr_to_loaded_data(attr_name: str,
-                             data: sc.Variable,
-                             value: np.ndarray,
-                             unit: sc.Unit,
-                             dtype: Optional[Any] = None):
-    try:
-        data = data.attrs
-    except AttributeError:
-        pass
-
-    try:
-        if dtype is not None:
-            if dtype == sc.DType.vector3:
-                data[attr_name] = sc.vector(value=value, unit=unit)
-            elif dtype == sc.DType.affine_transform3:
-                data[attr_name] = affine_transform(value=value, unit=unit)
-            elif dtype == sc.DType.linear_transform3:
-                data[attr_name] = linear_transform(value=value, unit=unit)
-            elif dtype == sc.DType.rotation3:
-                if unit != sc.units.one:
-                    raise sc.UnitError(
-                        f'Rotations must be dimensionless, got unit {unit}')
-                data[attr_name] = rotation(value=value)
-            elif dtype == sc.DType.translation3:
-                data[attr_name] = translation(value=value, unit=unit)
-            else:
-                data[attr_name] = sc.scalar(value=value, dtype=dtype, unit=unit)
-        else:
-            data[attr_name] = sc.scalar(value=value, unit=unit)
-    except KeyError:
-        pass
 
 
 def convert_time_to_datetime64(
