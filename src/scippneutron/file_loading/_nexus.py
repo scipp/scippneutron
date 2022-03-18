c73fd3b7cb9e0e83fc9f980abde1d2038aab5e1c
Simon Heybrock <simon.heybrock@ess.eu>
2022-03-18 06:44:25 +0100

    Remove unused

--- src/scippneutron/file_loading/_nexus.py
+++ src/scippneutron/file_loading/_nexus.py
@@ -1,9 +1,7 @@
 from ._json_nexus import LoadFromJson
 from ._hdf5_nexus import LoadFromHdf5
-from typing import Union, Dict
-import h5py
+from typing import Union
 import scipp as sc
 
 LoadFromNexus = Union[LoadFromJson, LoadFromHdf5]
-GroupObject = Union[h5py.Group, Dict]
 ScippData = Union[sc.Dataset, sc.DataArray, None]
