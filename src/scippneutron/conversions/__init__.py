ab43e632847e6f6b15893190573bbdad34ea9c2f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-08-16 15:50:45 +0200

    Add docstring to conversions module

--- src/scippneutron/conversions/__init__.py
+++ src/scippneutron/conversions/__init__.py
@@ -4,5 +4,14 @@
 
 # flake8: noqa[F401]
 
+"""Components for coordinate transformations.
+
+The submodules can be used to construct graphs for ``scipp.transform_coords``.
+Or they can be used independently.
+
+See also the user guide on
+`Coordinate Transformations <../../user-guide/coordinate-transformations.rst>`_.
+"""
+
 from . import beamline
 from . import tof
