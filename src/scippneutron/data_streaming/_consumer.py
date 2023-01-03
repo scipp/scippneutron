90015fb6518c4d7ed726ecb21bd57d0fe3154497
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2023-01-03 11:25:47 +0100

    Update copyright year

--- src/scippneutron/data_streaming/_consumer.py
+++ src/scippneutron/data_streaming/_consumer.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 from confluent_kafka import Consumer, TopicPartition, KafkaError
 from typing import Callable, List, Union, Optional, Tuple
 from warnings import warn
