89251be7240e4d60c483b4ad0ed4919b9bc1712a
Simon Heybrock <simon.heybrock@ess.eu>
2023-01-27 13:38:35 +0100

    Draft updated graph with pulse-skipping

--- src/scippneutron/tof/frames.py
+++ src/scippneutron/tof/frames.py
@@ -16,25 +16,45 @@ def _tof_from_wavelength(*, wavelength, Ltotal):
     return as_float_type(Ltotal * scale, wavelength) * wavelength
 
 
-def _tof_to_time_offset(*, tof, frame_length, frame_offset):
+def _tof_to_time_offset(*, tof, frame_period, frame_offset):
     unit = tof.unit
-    frame_length = frame_length.to(unit=unit)
+    frame_period = frame_period.to(unit=unit)
     arrival_time_offset = frame_offset.to(unit=unit) + tof
-    time_offset = arrival_time_offset % frame_length
+    time_offset = arrival_time_offset % frame_period
     return time_offset
 
 
-def _time_offset_to_tof(*, time_offset, time_offset_pivot, tof_min, frame_length):
-    frame_length = frame_length.to(unit=elem_unit(time_offset))
+def _time_offset_to_tof(*, time_offset, time_offset_pivot, tof_min, frame_period):
+    frame_period = frame_period.to(unit=elem_unit(time_offset))
     time_offset_pivot = time_offset_pivot.to(unit=elem_unit(time_offset))
     tof_min = tof_min.to(unit=elem_unit(time_offset))
     shift = tof_min - time_offset_pivot
-    tof = sc.where(time_offset >= time_offset_pivot, shift, shift + frame_length)
+    tof = sc.where(time_offset >= time_offset_pivot, shift, shift + frame_period)
     tof += time_offset
     return tof
 
 
-def to_tof():
+def time_zero_to_detection_frame_index(*, frame_offset, tof_min, frame_period,
+                                       pulse_stride, event_time_zero):
+    """
+    Return 0-based source frame index of detection frame.
+
+    The source frames containing the time marked by tof_min receive index 0.
+    The frame after that index 1, and so on, until frame_stride-1.
+    """
+    shift = frame_period * ((frame_offset + tof_min) // frame_period)
+    return ((event_time_zero + shift) // (frame_period)).value % pulse_stride
+
+
+def update_time_offset_for_pulse_skipping(event_time_offset, pulse_index, pulse_period):
+    return event_time_offset + pulse_index * pulse_period
+
+
+def pulse_to_frame(pulse_period: sc.Variable, pulse_stride: int) -> sc.Variable:
+    return pulse_period * pulse_stride
+
+
+def to_tof(*, pulse_skipping: bool = False):
     """
     Return a graph suitable for :py:func:`scipp.transform_coords` to convert frames
     to time-of-flight.
@@ -43,6 +63,7 @@ def to_tof():
 
     - event_time_zero and event_time_offset as read from an NXevent_data group.
     - lambda_min, used as a proxy for defining where to split and unwrap frames.
+    - pulse_period
     - frame_length and frame_offset defining the frame structure.
 
     This assumes elastic scattering, or at least that the gaps between frames arriving
@@ -53,21 +74,29 @@ def to_tof():
     def _tof_min(*, Ltotal, lambda_min):
         return _tof_from_wavelength(Ltotal=Ltotal, wavelength=lambda_min)
 
-    def _time_offset_pivot(*, tof_min, frame_length, frame_offset):
+    def _time_offset_pivot(*, tof_min, frame_period, frame_offset):
         return _tof_to_time_offset(tof=tof_min,
-                                   frame_length=frame_length,
+                                   frame_period=frame_period,
                                    frame_offset=frame_offset)
 
-    def _tof(*, event_time_offset, time_offset_pivot, tof_min, frame_length):
+    def _tof(*, event_time_offset, time_offset_pivot, tof_min, frame_period):
         return _time_offset_to_tof(time_offset=event_time_offset,
                                    time_offset_pivot=time_offset_pivot,
                                    tof_min=tof_min,
-                                   frame_length=frame_length)
+                                   frame_period=frame_period)
 
     graph = {}
+    if pulse_skipping:
+        graph['frame_period'] = pulse_to_frame
+        graph['time_offset'] = update_time_offset_for_pulse_skipping
+        graph['pulse_index'] = time_zero_to_detection_frame_index
+    else:
+        graph['frame_period'] = 'pulse_period'
+        graph['time_offset'] = 'event_time_offset'
+
     graph['tof_min'] = _tof_min
     graph['time_offset_pivot'] = _time_offset_pivot
-    graph['tof'] = _tof
+    graph['tof'] = _time_offset_to_tof
     return graph
 
 
@@ -85,15 +114,3 @@ def make_frames(da, *, frame_length, frame_offset=None, lambda_min=None):
         raise ValueError("Coordinate 'tof' already defined in input data array. "
                          "Expected input with 'event_time_offset' coordinate.")
     return da.transform_coords('tof', graph=graph)
-
-
-def time_zero_to_detection_frame_index(*, frame_offset, tof_min, frame_length,
-                                       frame_stride, time_zero):
-    """
-    Return 0-based source frame index of detection frame.
-
-    The source frames containing the time marked by tof_min receive index 0.
-    The frame after that index 1, and so on, until frame_stride-1.
-    """
-    shift = frame_length * ((frame_offset + tof_min) // frame_length)
-    return ((time_zero + shift) // (frame_length)).value % frame_stride
