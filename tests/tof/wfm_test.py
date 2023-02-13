df33baa0a6e2f59adb4bec8f0417fba3c7065811
Simon Heybrock <simon.heybrock@ess.eu>
2023-02-13 14:07:14 +0100

    More tests

--- tests/tof/wfm_test.py
+++ tests/tof/wfm_test.py
@@ -4,10 +4,9 @@
 from typing import Tuple
 
 import numpy as np
-import pytest
 import scipp as sc
 
-from scippneutron.conversion.graph import beamline
+from scippneutron.conversion import graph
 from scippneutron.tof import wfm
 from scippneutron.tof.frames import _tof_from_wavelength
 
@@ -47,11 +46,18 @@ def make_frame_data(*, source_position: sc.Variable, wav_min: sc.Variable,
                         wav_min=wav_min[1].values,
                         wav_max=wav_max[1].values,
                         source_position=source_position)
-    graph = beamline.beamline(scatter=True)
-    graph['tof'] = _tof_from_wavelength
-    frame1 = frame1.transform_coords('tof', graph=graph)
-    frame2 = frame2.transform_coords('tof', graph=graph)
+    g = graph.beamline.beamline(scatter=True)
+    g['tof'] = _tof_from_wavelength
+    frame1 = frame1.transform_coords('tof', graph=g)
+    frame2 = frame2.transform_coords('tof', graph=g)
     tof_edges = sc.linspace('tof', 0.0, 100000.0, num=1000, unit='us')
+    expected_wav = frame1.bins.concatenate(frame2)
+    wav_edges = sc.linspace('wavelength',
+                            wav_min.min().value,
+                            wav_max.max().value,
+                            num=1000,
+                            unit='angstrom')
+    expected_wav = expected_wav.hist(wavelength=wav_edges)
     expected = frame1.hist(tof=tof_edges)
     expected += frame2.hist(tof=tof_edges)
     # Consistency check: Are all TOFs within edges?
@@ -61,12 +67,13 @@ def make_frame_data(*, source_position: sc.Variable, wav_min: sc.Variable,
     # Consistency check: Params must be such that frames do not overlap
     assert sc.all(frame1.bins.coords['tof'].max() < frame2.bins.coords['tof'].min())
     frames = frame1.bins.concatenate(frame2)
-    da = frames.drop_attrs(['source_position', 'incident_beam', 'L1', 'Ltotal'])
+    da = frames.drop_attrs(
+        ['source_position', 'incident_beam', 'scattered_beam', 'L1', 'L2', 'Ltotal'])
     del da.bins.attrs['wavelength']
-    return da, expected
+    return da, expected_wav
 
 
-def test_x():
+def test_stitched_data_yields_correct_wavelength_histogram():
     source_position = sc.vector([1.0, 2.0, -70.0], unit='m')
     # note the overlap
     wav_min = sc.array(dims=['subframe'], values=[1.0, 2.9], unit='angstrom')
@@ -79,15 +86,97 @@ def test_x():
     stitched = wfm.stitch(da,
                           wavelength_min=wav_min,
                           wavelength_max=wav_max,
-                          subframe_source_position=1.0 * source_position,
+                          subframe_source_position=source_position,
                           subframe_offset=subframe_offset)
-    hist = stitched.hist(tof=expected.coords['tof'])
-    print(expected.data, hist.data)
+    # The stitched result is does not contain wavelength, but coords are setup to
+    # yield correct result.
+    stitched_wavelength = stitched.transform_coords('wavelength',
+                                                    graph=graph.tof.kinematic("tof"))
+    hist = stitched_wavelength.hist(wavelength=expected.coords['wavelength'])
     assert sc.identical(expected.data, hist.data)
-    #da.coords['tof'] = sc.scalar(1.0, unit='ms')
-    #with pytest.raises(ValueError):
-    #    unwrap_frames(da,
-    #                  scatter=True,
-    #                  pulse_period=71.0 * sc.Unit('ms'),
-    #                  frame_offset=30.1 * sc.Unit('ms'),
-    #                  lambda_min=2.5 * sc.Unit('Angstrom'))
+
+
+def test_stitch_incorrect_given_bad_subframe_source_position():
+    source_position = sc.vector([1.0, 2.0, -70.0], unit='m')
+    # note the overlap
+    wav_min = sc.array(dims=['subframe'], values=[1.0, 2.9], unit='angstrom')
+    wav_max = sc.array(dims=['subframe'], values=[3.0, 5.0], unit='angstrom')
+    subframe_offset = sc.array(dims=['subframe'], values=[1234.0, 4567.0], unit='us')
+    da, expected = make_frame_data(source_position=source_position,
+                                   wav_min=wav_min,
+                                   wav_max=wav_max,
+                                   subframe_offset=subframe_offset)
+    stitched = wfm.stitch(da,
+                          wavelength_min=wav_min,
+                          wavelength_max=wav_max,
+                          subframe_source_position=1.1 * source_position,
+                          subframe_offset=subframe_offset)
+    stitched_wavelength = stitched.transform_coords('wavelength',
+                                                    graph=graph.tof.kinematic("tof"))
+    hist = stitched_wavelength.hist(wavelength=expected.coords['wavelength'])
+    assert not sc.identical(expected.data, hist.data)
+
+
+def test_stitch_incorrect_given_bad_subframe_offset():
+    source_position = sc.vector([1.0, 2.0, -70.0], unit='m')
+    # note the overlap
+    wav_min = sc.array(dims=['subframe'], values=[1.0, 2.9], unit='angstrom')
+    wav_max = sc.array(dims=['subframe'], values=[3.0, 5.0], unit='angstrom')
+    subframe_offset = sc.array(dims=['subframe'], values=[1234.0, 4567.0], unit='us')
+    da, expected = make_frame_data(source_position=source_position,
+                                   wav_min=wav_min,
+                                   wav_max=wav_max,
+                                   subframe_offset=subframe_offset)
+    stitched = wfm.stitch(da,
+                          wavelength_min=wav_min,
+                          wavelength_max=wav_max,
+                          subframe_source_position=source_position,
+                          subframe_offset=1.05 * subframe_offset)
+    stitched_wavelength = stitched.transform_coords('wavelength',
+                                                    graph=graph.tof.kinematic("tof"))
+    hist = stitched_wavelength.hist(wavelength=expected.coords['wavelength'])
+    assert not sc.identical(expected.data, hist.data)
+
+
+def test_stitch_incorrect_given_bad_positions():
+    source_position = sc.vector([1.0, 2.0, -70.0], unit='m')
+    # note the overlap
+    wav_min = sc.array(dims=['subframe'], values=[1.0, 2.9], unit='angstrom')
+    wav_max = sc.array(dims=['subframe'], values=[3.0, 5.0], unit='angstrom')
+    subframe_offset = sc.array(dims=['subframe'], values=[1234.0, 4567.0], unit='us')
+    da, expected = make_frame_data(source_position=source_position,
+                                   wav_min=wav_min,
+                                   wav_max=wav_max,
+                                   subframe_offset=subframe_offset)
+    da.attrs['position'] = da.attrs['position']['pixel', 0]
+    stitched = wfm.stitch(da,
+                          wavelength_min=wav_min,
+                          wavelength_max=wav_max,
+                          subframe_source_position=source_position,
+                          subframe_offset=subframe_offset)
+    stitched_wavelength = stitched.transform_coords('wavelength',
+                                                    graph=graph.tof.kinematic("tof"))
+    hist = stitched_wavelength.hist(wavelength=expected.coords['wavelength'])
+    assert not sc.identical(expected.data, hist.data)
+
+
+def test_stitch_incorrect_given_bad_wavelength_bounds():
+    source_position = sc.vector([1.0, 2.0, -70.0], unit='m')
+    # note the overlap
+    wav_min = sc.array(dims=['subframe'], values=[1.0, 2.9], unit='angstrom')
+    wav_max = sc.array(dims=['subframe'], values=[3.0, 5.0], unit='angstrom')
+    subframe_offset = sc.array(dims=['subframe'], values=[1234.0, 4567.0], unit='us')
+    da, expected = make_frame_data(source_position=source_position,
+                                   wav_min=wav_min,
+                                   wav_max=wav_max,
+                                   subframe_offset=subframe_offset)
+    wav_max[0] *= 0.9
+    stitched = wfm.stitch(da,
+                          wavelength_min=wav_min,
+                          wavelength_max=wav_max,
+                          subframe_source_position=source_position,
+                          subframe_offset=subframe_offset)
+    stitched_wavelength = stitched.transform_coords('wavelength',
+                                                    graph=graph.tof.kinematic("tof"))
+    hist = stitched_wavelength.hist(wavelength=expected.coords['wavelength'])
+    assert not sc.identical(expected.data, hist.data)
