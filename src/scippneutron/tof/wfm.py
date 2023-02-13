e2c90b9c13329a871fcf5d50c20ffdcd26ca6675
Simon Heybrock <simon.heybrock@ess.eu>
2023-02-13 10:20:44 +0100

    Docstrings

--- src/scippneutron/tof/wfm.py
+++ src/scippneutron/tof/wfm.py
@@ -12,10 +12,11 @@ from ..conversion.graph import beamline
 from .frames import _tof_from_wavelength
 
 
-def subframe_time_bounds_from_wavelengths(Ltotal: sc.Variable,
-                                          wavelength_min: sc.Variable,
-                                          wavelength_max: sc.Variable,
-                                          subframe_offset: sc.Variable) -> sc.Variable:
+def _subframe_time_bounds_from_wavelengths(Ltotal: sc.Variable,
+                                           wavelength_min: sc.Variable,
+                                           wavelength_max: sc.Variable,
+                                           subframe_offset: sc.Variable) -> sc.Variable:
+    """Compute bounds of subframes for cutting data."""
     dummy = uuid.uuid4().hex
     dims = [subframe_offset.dim, dummy]
     wavelength = sc.concat([wavelength_min, wavelength_max], dummy).transpose(dims)
@@ -24,7 +25,7 @@ def subframe_time_bounds_from_wavelengths(Ltotal: sc.Variable,
     return time_bounds.flatten(dims=dims, to='tof')
 
 
-def incident_beam_from_subframe_source_position(
+def _incident_beam_from_subframe_source_position(
         subframe_source_position: sc.Variable,
         sample_position: sc.Variable) -> sc.Variable:
     """Vector from subframe source (such as WFM chopper) to sample"""
@@ -33,6 +34,7 @@ def incident_beam_from_subframe_source_position(
 
 def cut_and_offset_subframes(da: sc.DataArray, subframe_bounds: sc.Variable,
                              subframe_offset: sc.Variable) -> sc.DataArray:
+    """Cut and offset subframes of event data based on known bounds and offsets."""
     dim = 'tof'
     binned = da.bin({dim: subframe_bounds})
     subframe_offset = subframe_offset.rename({subframe_offset.dim: dim})
@@ -44,13 +46,44 @@ def cut_and_offset_subframes(da: sc.DataArray, subframe_bounds: sc.Variable,
 def stitch(da: sc.DataArray, wavelength_min: sc.Variable, wavelength_max: sc.Variable,
            subframe_source_position: sc.Variable,
            subframe_offset: sc.Variable) -> sc.DataArray:
+    """
+    Stitch WFM subframes of unstitched input data.
+
+    We refer to the stitched components as "subframes", as opposed to the "frames" that
+    originate from different pulses.
+
+    Parameters
+    ----------
+    da:
+        Input data with raw 'tof'.
+    wavelength_min:
+        Minimum wavelengths for each subframe.
+    wavelength_max:
+        Maximum wavelengths for each subframe.
+    subframe_source_position:
+        Position of the subframe source. Typically the position of the WFM chopper, or
+        the center point if there are two WFM choppers. Precise location depends on the
+        concrete design of the chopper cascade.
+    subframe_offset:
+        Time offset for each subframe. This is the time difference between the base time
+        of the input's 'tof' coordinate and the base time for each subframe. This is
+        essentially the time offset of the WFM chopper openings.
+
+    Returns
+    -------
+    :
+        Data with corrected 'tof' coordinate. Furthermore, the 'incident_beam'
+        coordinate is set based on `subframe_source_position` such that subsequent
+        calculations of flight-path-length-dependent quantities will be correct
+        when using the standard conversion graphs.
+    """
     da = da.copy(deep=False)
     da.coords['subframe_source_position'] = subframe_source_position
     graph = beamline.beamline(scatter=True)
-    graph['incident_beam'] = incident_beam_from_subframe_source_position
+    graph['incident_beam'] = _incident_beam_from_subframe_source_position
     da = da.transform_coords('Ltotal', graph=graph)
 
-    subframe_bounds = subframe_time_bounds_from_wavelengths(
+    subframe_bounds = _subframe_time_bounds_from_wavelengths(
         Ltotal=da.coords['Ltotal'],
         wavelength_min=wavelength_min,
         wavelength_max=wavelength_max,
