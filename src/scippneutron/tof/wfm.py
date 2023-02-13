a89e195825b976f522c75e9dbaa72bcd24b9fc3f
Simon Heybrock <simon.heybrock@ess.eu>
2023-02-13 10:05:17 +0100

    Cleanup

--- src/scippneutron/tof/wfm.py
+++ src/scippneutron/tof/wfm.py
@@ -8,15 +8,14 @@ import uuid
 
 import scipp as sc
 
+from ..conversion.graph import beamline
 from .frames import _tof_from_wavelength
 
 
-def subframe_time_bounds_from_wavelengths(
-        wavelength_min: sc.Variable, wavelength_max: sc.Variable,
-        sample_position: sc.Variable, L2: sc.Variable, subframe_offset: sc.Variable,
-        subframe_source_position: sc.Variable) -> sc.Variable:
-    L1 = sc.norm(sample_position - subframe_source_position)
-    Ltotal = L1 + L2
+def subframe_time_bounds_from_wavelengths(Ltotal: sc.Variable,
+                                          wavelength_min: sc.Variable,
+                                          wavelength_max: sc.Variable,
+                                          subframe_offset: sc.Variable) -> sc.Variable:
     dummy = uuid.uuid4().hex
     dims = [subframe_offset.dim, dummy]
     wavelength = sc.concat([wavelength_min, wavelength_max], dummy).transpose(dims)
@@ -25,10 +24,37 @@ def subframe_time_bounds_from_wavelengths(
     return time_bounds.flatten(dims=dims, to='tof')
 
 
-def stitch(da: sc.DataArray, subframe_bounds: sc.Variable,
-           subframe_offset: sc.Variable) -> sc.DataArray:
+def incident_beam_from_subframe_source_position(
+        subframe_source_position: sc.Variable,
+        sample_position: sc.Variable) -> sc.Variable:
+    """Vector from subframe source (such as WFM chopper) to sample"""
+    return sample_position - subframe_source_position
+
+
+def cut_and_offset_subframes(da: sc.DataArray, subframe_bounds: sc.Variable,
+                             subframe_offset: sc.Variable) -> sc.DataArray:
     dim = 'tof'
     binned = da.bin({dim: subframe_bounds})
-    binned.bins.coords[dim][dim, ::2] -= subframe_offset.rename(subframe=dim)
+    subframe_offset = subframe_offset.rename({subframe_offset.dim: dim})
+    binned.bins.coords[dim][dim, ::2] -= subframe_offset
     del binned.coords[dim]
     return binned[dim, ::2].bins.concat(dim)
+
+
+def stitch(da: sc.DataArray, wavelength_min: sc.Variable, wavelength_max: sc.Variable,
+           subframe_source_position: sc.Variable,
+           subframe_offset: sc.Variable) -> sc.DataArray:
+    da = da.copy(deep=False)
+    da.coords['subframe_source_position'] = subframe_source_position
+    graph = beamline.beamline(scatter=True)
+    graph['incident_beam'] = incident_beam_from_subframe_source_position
+    da = da.transform_coords('Ltotal', graph=graph)
+
+    subframe_bounds = subframe_time_bounds_from_wavelengths(
+        Ltotal=da.coords['Ltotal'],
+        wavelength_min=wavelength_min,
+        wavelength_max=wavelength_max,
+        subframe_offset=subframe_offset)
+    return cut_and_offset_subframes(da,
+                                    subframe_bounds=subframe_bounds,
+                                    subframe_offset=subframe_offset)
