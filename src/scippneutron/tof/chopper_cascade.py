e18b9f4a4f6f682b811f0fd81c50bf33cb6cfc73
Simon Heybrock <simon.heybrock@ess.eu>
2023-11-21 14:17:52 +0100

    Comment on Frame.subbounds

--- src/scippneutron/tof/chopper_cascade.py
+++ src/scippneutron/tof/chopper_cascade.py
@@ -203,6 +203,11 @@ class Frame:
         This is not the same as the bounds of the individual subframes, but defined as
         the union of all subframes. Subframes that overlap in time are "merged" into a
         single subframe.
+
+        This function is to some extent experimental: It is not clear if taking the
+        union of overlapping subframes has any utility in practice, since this may
+        simply indicate a problem with the chopper cascade. Attempts to handle this
+        automatically may be misleading.
         """
         starts = [subframe.start_time for subframe in self.subframes]
         ends = [subframe.end_time for subframe in self.subframes]
