617993100e7e9074150daa91a8542fcb24b03004
Simon Heybrock <simon.heybrock@ess.eu>
2023-11-28 11:29:02 +0100

    Cleanup

--- src/scippneutron/tof/unwrap.py
+++ src/scippneutron/tof/unwrap.py
@@ -37,72 +37,6 @@ TofData = NewType('TofData', sc.DataArray)
 UnwrappedData = NewType('UnwrappedData', sc.DataArray)
 
 
-# Should this be a helper, or a provider?
-def _time_offset(
-    *,
-    wrapped_time_offset: sc.Variable,
-    time_offset_min: sc.Variable,
-    frame_period: sc.Variable,
-) -> sc.Variable:
-    """
-    Time offset from the start of the frame emitting the neutron.
-
-    This is not identical to the time-of-flight, since the time-of-flight is measured,
-    e.g., from the center of the pulse, to the center of a pulse-shaping chopper slit.
-
-    Parameters
-    ----------
-    wrapped_time_offset :
-        Time offset from the time-zero as recorded by the data acquisition system.
-    time_offset_min :
-        Minimum arrival time offset of neutrons that can pass through the chopper
-        cascade. Typically pixel-dependent.
-    frame_period :
-        Time between the start of two consecutive frames, i.e., the period of the
-        time-zero used by the data acquisition system.
-    """
-    wrapped_time_min = time_offset_min % frame_period
-    delta = frame_period if wrapped_time_offset < wrapped_time_min else 0
-    offset_frames = time_offset_min - wrapped_time_min + delta
-    return offset_frames + wrapped_time_offset
-
-
-def _time_of_flight(
-    *,
-    time_offset: sc.Variable,
-    source_time_open: sc.Variable,
-    source_time_close: sc.Variable,
-) -> sc.Variable:
-    """
-    Time-of-flight of neutrons passing through a chopper cascade.
-
-    A chopper is used to define (1) the "source" location and (2) the time-of-flight
-    time origin. The time-of-flight is then the time difference between the time of
-    arrival of the neutron at the detector, and the time of arrival of the neutron at
-    the chopper. L1 needs to be redefined to be the distance between the chopper and
-    the sample.
-
-    If there is no pulse-shaping chopper, then the source-pulse begin and end time
-    should be set as the source_time_open and source_time_close, respectively.
-
-    For WFM, the source_time_open and source_time_close will be different for each
-    subframe. In this case, all input parameters should be given as variables with
-    subframe as dimension.
-
-    Parameters
-    ----------
-    time_offset :
-        Time offset from the start of the frame emitting the neutron.
-    source_time_open :
-        Time at which the source chopper opens.
-    source_time_close :
-        Time at which the source chopper closes.
-    """
-    # TODO Need to handle choppers with multiple openings, where we need to select one
-    time_zero = 0.5 * (source_time_open + source_time_close)
-    return time_offset - time_zero
-
-
 def frame_period(
     pulse_period: PulsePeriod, pulse_stride: Optional[PulseStride]
 ) -> FramePeriod:
@@ -190,16 +124,32 @@ def time_offset(
     frame_period: FramePeriod,
 ) -> TimeOffset:
     """
+    Time offset from the start of the frame emitting the neutron.
+
+    This is not identical to the time-of-flight, since the time-of-flight is measured,
+    e.g., from the center of the pulse, to the center of a pulse-shaping chopper slit.
+
     Note that this takes the frame-period, which can be a multiple of a pulse-period.
     The input time stamps must be relative the the start of the frame, i.e., in
     pulse-skipping mode a pulse offset must have been applied before calling this
     function.
+
+    Parameters
+    ----------
+    wrapped_time_offset :
+        Time offset from the time-zero as recorded by the data acquisition system.
+    time_offset_min :
+        Minimum arrival time offset of neutrons that can pass through the chopper
+        cascade. Typically pixel-dependent.
+    frame_period :
+        Time between the start of two consecutive frames, i.e., the period of the
+        time-zero used by the data acquisition system.
     """
-    return _time_offset(
-        wrapped_time_offset=wrapped_time_offset,
-        time_offset_min=frame_bounds['bound', 0],
-        frame_period=frame_period,
-    )
+    time_offset_min = frame_bounds['bound', 0]
+    wrapped_time_min = time_offset_min % frame_period
+    delta = frame_period if wrapped_time_offset < wrapped_time_min else 0
+    offset_frames = time_offset_min - wrapped_time_min + delta
+    return offset_frames + wrapped_time_offset
 
 
 def source_chopper(
@@ -212,11 +162,34 @@ def time_of_flight(
     time_offset: TimeOffset,
     source_chopper: SourceChopper,
 ) -> TimeOfFlight:
-    return _time_of_flight(
-        time_offset=time_offset,
-        source_time_open=source_chopper.time_open,
-        source_time_close=source_chopper.time_close,
-    )
+    """
+    Time-of-flight of neutrons passing through a chopper cascade.
+
+    A chopper is used to define (1) the "source" location and (2) the time-of-flight
+    time origin. The time-of-flight is then the time difference between the time of
+    arrival of the neutron at the detector, and the time of arrival of the neutron at
+    the chopper. L1 needs to be redefined to be the distance between the chopper and
+    the sample.
+
+    If there is no pulse-shaping chopper, then the source-pulse begin and end time
+    should be set as the source_time_open and source_time_close, respectively.
+
+    For WFM, the source_time_open and source_time_close will be different for each
+    subframe. In this case, all input parameters should be given as variables with
+    subframe as dimension.
+
+    Parameters
+    ----------
+    time_offset :
+        Time offset from the start of the frame emitting the neutron.
+    source_chopper :
+        Chopper defining the source location and time-of-flight time origin.
+    """
+    source_time_open = source_chopper.time_open
+    source_time_close = source_chopper.time_close
+    # TODO Need to handle choppers with multiple openings, where we need to select one
+    time_zero = 0.5 * (source_time_open + source_time_close)
+    return time_offset - time_zero
 
 
 def time_of_flight_wfm(
