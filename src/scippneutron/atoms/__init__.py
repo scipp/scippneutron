48faefa5c4a9b99cfd18dd152314e3f62497c1fd
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-20 10:41:45 +0100

    Use dataclass for ScatteringParams

--- src/scippneutron/atoms/__init__.py
+++ src/scippneutron/atoms/__init__.py
@@ -1,10 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 """Parameters for neutron interactions with atoms."""
+from __future__ import annotations
 
+import dataclasses
 import importlib.resources
 from functools import lru_cache
-from typing import Optional, TextIO
+from typing import Optional, TextIO, Union
 
 import scipp as sc
 
@@ -20,50 +22,73 @@ def reference_wavelength() -> sc.Variable:
     return sc.scalar(1.7982, unit='angstrom')
 
 
-@lru_cache()
-def scattering_params(isotope: str) -> dict[str, Optional[sc.Variable]]:
-    """Return the scattering parameters for the given element / isotope.
+@dataclasses.dataclass(frozen=True, eq=False)
+class ScatteringParams:
+    """Scattering parameters for neutrons with a specific element / isotope.
 
     Provides access to the scattering lengths and cross-sections of neutrons
     with a given element or isotope.
     Values have been retrieved at 2024-02-19T17:00:00Z from the list at
     https://www.ncnr.nist.gov/resources/n-lengths/list.html
     which is based on :cite:`sears:1992`.
+    Values are ``None`` where the table does not provide values.
 
     The absorption cross-section applies to neutrons with a wavelength
     of 1.7982 Å.
     See :func:`reference_wavelength`.
-
-    Parameters
-    ----------
-    isotope:
-        Name of the element or isotope.
-        For example, 'H', '3He', 'V', '50V'.
-
-    Returns
-    -------
-    :
-        Dict with the scattering parameters.
-        A value can be ``None`` if the parameter is not available.
-        Scattering lengths are split into real and imaginary parts.
-
-        Keys:
-
-        - ``bound_coherent_scattering_length_re``
-        - ``bound_coherent_scattering_length_im``
-        - ``bound_incoherent_scattering_length_re``
-        - ``bound_incoherent_scattering_length_im``
-        - ``bound_coherent_scattering_cross_section``
-        - ``bound_incoherent_scattering_cross_section``
-        - ``total_bound_scattering_cross_section``
-        - ``absorption_cross_section``
     """
-    with _open_scattering_parameters_file() as f:
-        while line := f.readline():
-            name, rest = line.split(',', 1)
-            if name == isotope:
-                return _parse_line(rest)
-    raise ValueError(f"No entry for element / isotope '{isotope}'")
+
+    isotope: str
+    """Element / isotope name."""
+    coherent_scattering_length_re: Optional[sc.Variable]
+    """Bound coherent scattering length (real part)."""
+    coherent_scattering_length_im: Optional[sc.Variable]
+    """Bound coherent scattering length (imaginary part)."""
+    incoherent_scattering_length_re: Optional[sc.Variable]
+    """Bound incoherent scattering length (real part)."""
+    incoherent_scattering_length_im: Optional[sc.Variable]
+    """Bound incoherent scattering length (imaginary part)."""
+    coherent_scattering_cross_section: Optional[sc.Variable]
+    """Bound coherent scattering cross-section."""
+    incoherent_scattering_cross_section: Optional[sc.Variable]
+    """Bound incoherent scattering cross-section."""
+    total_scattering_cross_section: Optional[sc.Variable]
+    """Total bound scattering cross-section."""
+    absorption_cross_section: Optional[sc.Variable]
+    """Absorption cross-section for λ = 1.7982 Å neutrons."""
+
+    def __eq__(self, other: object) -> Union[bool, type(NotImplemented)]:
+        if not isinstance(other, ScatteringParams):
+            return NotImplemented
+        return all(
+            self.isotope == other.isotope
+            if field.name == 'isotope'
+            else _eq_or_identical(getattr(self, field.name), getattr(other, field.name))
+            for field in dataclasses.fields(self)
+        )
+
+    @staticmethod
+    @lru_cache()
+    def for_isotope(isotope: str) -> ScatteringParams:
+        """Return the scattering parameters for the given element / isotope.
+
+        Parameters
+        ----------
+        isotope:
+            Name of the element or isotope.
+            For example, 'H', '3He', 'V', '50V'.
+
+        Returns
+        -------
+        :
+            Neutron scattering parameters.
+        """
+        with _open_scattering_parameters_file() as f:
+            while line := f.readline():
+                name, rest = line.split(',', 1)
+                if name == isotope:
+                    return _parse_line(isotope, rest)
+        raise ValueError(f"No entry for element / isotope '{isotope}'")
 
 
 def _open_scattering_parameters_file() -> TextIO:
@@ -74,28 +99,21 @@ def _open_scattering_parameters_file() -> TextIO:
     )
 
 
-def _parse_line(line: str) -> dict[str, Optional[sc.Variable]]:
+def _parse_line(isotope: str, line: str) -> ScatteringParams:
     line = line.rstrip().split(',')
-    return {
-        'bound_coherent_scattering_length_re': _assemble_scalar(line[0], line[1], 'fm'),
-        'bound_coherent_scattering_length_im': _assemble_scalar(line[2], line[3], 'fm'),
-        'bound_incoherent_scattering_length_re': _assemble_scalar(
-            line[4], line[5], 'fm'
-        ),
-        'bound_incoherent_scattering_length_im': _assemble_scalar(
-            line[6], line[7], 'fm'
-        ),
-        'bound_coherent_scattering_cross_section': _assemble_scalar(
-            line[8], line[9], 'barn'
-        ),
-        'bound_incoherent_scattering_cross_section': _assemble_scalar(
+    return ScatteringParams(
+        isotope=isotope,
+        coherent_scattering_length_re=_assemble_scalar(line[0], line[1], 'fm'),
+        coherent_scattering_length_im=_assemble_scalar(line[2], line[3], 'fm'),
+        incoherent_scattering_length_re=_assemble_scalar(line[4], line[5], 'fm'),
+        incoherent_scattering_length_im=_assemble_scalar(line[6], line[7], 'fm'),
+        coherent_scattering_cross_section=_assemble_scalar(line[8], line[9], 'barn'),
+        incoherent_scattering_cross_section=_assemble_scalar(
             line[10], line[11], 'barn'
         ),
-        'total_bound_scattering_cross_section': _assemble_scalar(
-            line[12], line[13], 'barn'
-        ),
-        'absorption_cross_section': _assemble_scalar(line[14], line[15], 'barn'),
-    }
+        total_scattering_cross_section=_assemble_scalar(line[12], line[13], 'barn'),
+        absorption_cross_section=_assemble_scalar(line[14], line[15], 'barn'),
+    )
 
 
 def _assemble_scalar(value: str, std: str, unit: str) -> Optional[sc.Variable]:
@@ -104,3 +122,9 @@ def _assemble_scalar(value: str, std: str, unit: str) -> Optional[sc.Variable]:
     value = float(value)
     variance = float(std) ** 2 if std else None
     return sc.scalar(value, variance=variance, unit=unit)
+
+
+def _eq_or_identical(a: Optional[sc.Variable], b: Optional[sc.Variable]) -> bool:
+    if a is None:
+        return b is None
+    return sc.identical(a, b)
--- tests/atoms/test_atoms.py
+++ tests/atoms/test_atoms.py
@@ -3,68 +3,48 @@
 
 import pytest
 import scipp as sc
-from scipp.testing import assert_identical
 
 import scippneutron as scn
 
 
 def test_scattering_params_h():
-    params = scn.atoms.scattering_params('H')
-    assert len(params) == 8
-    assert_identical(
-        params['bound_coherent_scattering_length_re'], sc.scalar(-3.739, unit='fm')
+    params = scn.atoms.ScatteringParams.for_isotope('H')
+    expected = scn.atoms.ScatteringParams(
+        isotope='H',
+        coherent_scattering_length_re=sc.scalar(-3.739, unit='fm'),
+        coherent_scattering_length_im=None,
+        incoherent_scattering_length_re=None,
+        incoherent_scattering_length_im=None,
+        coherent_scattering_cross_section=sc.scalar(1.7568, unit='barn'),
+        incoherent_scattering_cross_section=sc.scalar(80.26, unit='barn'),
+        total_scattering_cross_section=sc.scalar(82.02, unit='barn'),
+        absorption_cross_section=sc.scalar(0.3326, unit='barn'),
     )
-    assert params['bound_coherent_scattering_length_im'] is None
-    assert params['bound_incoherent_scattering_length_re'] is None
-    assert params['bound_incoherent_scattering_length_im'] is None
-    assert_identical(
-        params['bound_coherent_scattering_cross_section'],
-        sc.scalar(1.7568, unit='barn'),
-    )
-    assert_identical(
-        params['bound_incoherent_scattering_cross_section'],
-        sc.scalar(80.26, unit='barn'),
-    )
-    assert_identical(
-        params['total_bound_scattering_cross_section'], sc.scalar(82.02, unit='barn')
-    )
-    assert_identical(params['absorption_cross_section'], sc.scalar(0.3326, unit='barn'))
+    assert params == expected
 
 
 def test_scattering_params_157gd():
-    params = scn.atoms.scattering_params('157Gd')
-    assert len(params) == 8
-    assert_identical(
-        params['bound_coherent_scattering_length_re'], sc.scalar(-1.14, unit='fm')
+    params = scn.atoms.ScatteringParams.for_isotope('157Gd')
+    expected = scn.atoms.ScatteringParams(
+        isotope='157Gd',
+        coherent_scattering_length_re=sc.scalar(-1.14, unit='fm'),
+        coherent_scattering_length_im=sc.scalar(-71.9, unit='fm'),
+        incoherent_scattering_length_re=sc.scalar(5.0, variance=5.0**2, unit='fm'),
+        incoherent_scattering_length_im=sc.scalar(-55.8, unit='fm'),
+        coherent_scattering_cross_section=sc.scalar(
+            650.0, variance=4.0**2, unit='barn'
+        ),
+        incoherent_scattering_cross_section=sc.scalar(
+            394.0, variance=7.0**2, unit='barn'
+        ),
+        total_scattering_cross_section=sc.scalar(
+            1044.0, variance=8.0**2, unit='barn'
+        ),
+        absorption_cross_section=sc.scalar(259000.0, variance=700.0**2, unit='barn'),
     )
-    assert_identical(
-        params['bound_coherent_scattering_length_im'], sc.scalar(-71.9, unit='fm')
-    )
-    assert_identical(
-        params['bound_incoherent_scattering_length_re'],
-        sc.scalar(5.0, variance=5.0**2, unit='fm'),
-    )
-    assert_identical(
-        params['bound_incoherent_scattering_length_im'], sc.scalar(-55.8, unit='fm')
-    )
-    assert_identical(
-        params['bound_coherent_scattering_cross_section'],
-        sc.scalar(650.0, variance=4.0**2, unit='barn'),
-    )
-    assert_identical(
-        params['bound_incoherent_scattering_cross_section'],
-        sc.scalar(394.0, variance=7.0**2, unit='barn'),
-    )
-    assert_identical(
-        params['total_bound_scattering_cross_section'],
-        sc.scalar(1044.0, variance=8.0**2, unit='barn'),
-    )
-    assert_identical(
-        params['absorption_cross_section'],
-        sc.scalar(259000.0, variance=700.0**2, unit='barn'),
-    )
+    assert params == expected
 
 
 def test_scattering_params_unknown():
     with pytest.raises(ValueError):
-        scn.atoms.scattering_params('scippium')
+        scn.atoms.ScatteringParams.for_isotope('scippium')
