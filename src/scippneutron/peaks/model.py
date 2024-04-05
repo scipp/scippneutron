8e006adb0ab49f31acf45f7d394d172803288cde
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-04-05 10:38:17 +0200

    Mode peak models

--- src/scippneutron/peaks/model.py
+++ src/scippneutron/peaks/model.py
@@ -12,24 +12,64 @@ import scipp as sc
 
 class Model(abc.ABC):
     def __init__(self, *, prefix: str, param_names: Iterable[str]) -> None:
+        """Initialize a base model.
+
+        Parameters
+        ----------
+        prefix:
+            Prefix used for model parameters in all user-facing data.
+        param_names:
+            Names of parameters in arbitrary order.
+            Does not include the prefix.
+        """
         self._prefix = prefix
         self._param_names = set(param_names)
 
     @abc.abstractmethod
     def _call(self, x: sc.Variable, params: dict[str, sc.Variable]) -> sc.Variable:
+        """Evaluate the model at given independent variables and parameters.
+
+        The parameters are given *without* the prefix.
+        """
         ...
 
     @abc.abstractmethod
     def _guess(self, x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
+        """Roughly estimate the model parameters.
+
+        Parameters
+        ----------
+        x:
+            Independent variable.
+        y:
+            Dependent variable.
+
+        Returns
+        -------
+        :
+            Estimated parameters.
+            Dict keys are parameter names *without* the prefix.
+        """
         ...
 
+    @property
+    def prefix(self) -> str:
+        return self._prefix
+
+    @property
+    def param_names(self) -> set[str]:
+        """Parameter names including the prefix."""
+        return {self._prefix + name for name in self._param_names}
+
     def __call__(self, x: sc.Variable, **params: sc.Variable) -> sc.Variable:
-        if params.keys() != self._param_names:
+        if params.keys() != self.param_names:
             raise ValueError(
                 f'Bad parameters for model {self.__class__.__name__},'
                 f'got: {set(params.keys())}, expected {self._param_names}'
             )
-        return self._call(x, params)
+        return self._call(
+            x, {name[len(self._prefix) :]: val for name, val in params.items()}
+        )
 
     def guess(
         self, data: sc.DataArray, *, coord: str | None = None
@@ -42,44 +82,90 @@ class Model(abc.ABC):
         }
 
 
-class LinearModel(Model):
-    def __init__(self, *, prefix: str) -> None:
-        super().__init__(prefix=prefix, param_names=('slope', 'offset'))
+class PolynomialModel(Model):
+    def __init__(self, *, degree: int, prefix: str) -> None:
+        if degree <= 0:
+            raise ValueError(f'Degree must be positive, got: {degree}')
+        super().__init__(
+            prefix=prefix, param_names=(f'a{i}' for i in range(degree + 1))
+        )
+
+    @property
+    def degree(self) -> int:
+        return len(self._param_names) - 1
 
     def _call(self, x: sc.Variable, params: dict[str, sc.Variable]) -> sc.Variable:
-        slope = params['slope']
-        offset = params['offset']
-
-        val = slope * x
-        val += offset
+        # The loop is rather complex to ensure that `val` has the correct shape after
+        # broadcasting (assuming that all params have the same shape).
+        val = params[f'a{self.degree}'] * x
+        for i in range(self.degree - 1, 0, -1):
+            val += params[f'a{i}']
+            val *= x
+        val += params['a0']
         return val
 
     def _guess(self, x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
-        poly = np.polynomial.Polynomial.fit(x.values, y.values, deg=1)
-        offset, slope = poly.convert().coef
+        poly = np.polynomial.Polynomial.fit(x.values, y.values, deg=self.degree)
         return {
-            'offset': sc.scalar(offset, unit=y.unit),
-            'slope': sc.scalar(slope, unit=y.unit / x.unit),
+            f'a{i}': sc.scalar(c, unit=y.unit / x.unit**i)
+            for i, c in enumerate(poly.convert().coef)
         }
 
 
 class GaussianModel(Model):
     def __init__(self, *, prefix: str) -> None:
-        super().__init__(prefix=prefix, param_names=('amplitude', 'loc', 'width'))
+        super().__init__(prefix=prefix, param_names=('amplitude', 'loc', 'scale'))
 
     def _call(self, x: sc.Variable, params: dict[str, sc.Variable]) -> sc.Variable:
         amplitude = params['amplitude']
         loc = params['loc']
         scale = params['scale']
 
-        val = (x - loc) ** 2
+        # Avoid division by 0
+        scale = sc.scalar(
+            max(scale.value, 1e-15), variance=scale.variance, unit=scale.unit
+        )
+
+        val = -((x - loc) ** 2)
         val /= 2 * scale**2
         val = sc.exp(val, out=val)
         val *= amplitude / (math.sqrt(2 * math.pi) * scale)
         return val
 
     def _guess(self, x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
-        return {name: param for name, param in _guess_from_peak(x, y).items()}
+        params = {name: param for name, param in _guess_from_peak(x, y).items()}
+
+        params['amplitude'] *= math.sqrt(2 * math.pi)
+        return params
+
+
+class LorentzianModel(Model):
+    def __init__(self, *, prefix: str) -> None:
+        super().__init__(prefix=prefix, param_names=('amplitude', 'loc', 'scale'))
+
+    def _call(self, x: sc.Variable, params: dict[str, sc.Variable]) -> sc.Variable:
+        amplitude = params['amplitude']
+        loc = params['loc']
+        scale = params['scale']
+
+        # Use `max` to avoid division by 0
+        val = (x - loc) ** 2
+        val += (
+            sc.scalar(max(scale.value, 1e-15), variance=scale.variance, unit=scale.unit)
+            ** 2
+        )
+        val = sc.reciprocal(val, out=val)
+        val *= amplitude * scale / math.pi
+        return val
+
+    def _guess(self, x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
+        params = {name: param for name, param in _guess_from_peak(x, y).items()}
+        # Fudge factor taken from lmfit.
+        # Not sure where exactly it comes from, but it is related to the normalization
+        # of a Lorentzian and is approximately
+        # 3.0 * math.pi / math.sqrt(2 * math.pi)
+        params['amplitude'] *= 3.75
+        return params
 
 
 def _guess_from_peak(x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
@@ -98,15 +184,14 @@ def _guess_from_peak(x: sc.Variable, y: sc.Variable) -> dict[str, sc.Variable]:
     if len(x_half_max) > 2:
         loc = x_half_max.mean()
         # Rough estimate of sigma ~ FWHM / 2.355 for Gaussian.
+        # Exact gamma = FWHM / 2 for Lorentzian.
         scale = (x_half_max.max() - x_half_max.min()) / 2.0
     else:
         loc = x[np.argmax(y.values)]
         # 6.0 taken from lmfit, don't know where it comes from.
         scale = (sc.max(x) - sc.min(x)) / 6.0
 
-    # 3.0 approximates sqrt(2*pi) in the normalization of a Gaussian.
-    height = (y_max - y_min) * 3.0
-    amplitude = height * scale
+    amplitude = scale * (y_max - y_min)
     # TODO lower bound for sigma
     #   also for amplitude in the actual peak fit but not necessarily here
     return {
--- tests/peaks/model_test.py
+++ tests/peaks/model_test.py
@@ -4,44 +4,128 @@
 import math
 
 import numpy as np
+import pytest
 import scipp as sc
 import scipp.testing
 
 from scippneutron.peaks import model
 
 
-def test_linear_guess_params():
-    offset = sc.scalar(0.2, unit='cm')
-    slope = sc.scalar(-5.1, unit='cm/s')
+@pytest.mark.parametrize('prefix', ('', 'lorentz_'))
+def test_polynomial_degree_1_call(prefix: str):
+    a0 = sc.scalar(0.2, unit='cm')
+    a1 = sc.scalar(-5.1, unit='cm/s')
+    params = {f'{prefix}a0': a0, f'{prefix}a1': a1}
+    m = model.PolynomialModel(degree=1, prefix=prefix)
 
     x = sc.linspace('xx', 0.8, 4.3, 100, unit='s')
-    y = offset + slope * x
-    data = sc.DataArray(y, coords={'xx': x})
-
-    m = model.LinearModel(prefix='')
-    params = m.guess(data)
-    assert params.keys() == {'slope', 'offset'}
-    sc.testing.assert_allclose(params['slope'], slope, atol=sc.scalar(0.1, unit='cm/s'))
-    sc.testing.assert_allclose(params['offset'], offset, atol=sc.scalar(0.1, unit='cm'))
+    expected = a0 + a1 * x
+    actual = m(x, **params)
+    sc.testing.assert_allclose(actual, expected)
 
 
-def test_linear_guess_params_prefix():
-    offset = sc.scalar(0.2, unit='cm')
-    slope = sc.scalar(-5.1, unit='cm/s')
+@pytest.mark.parametrize('prefix', ('', 'lorentz_'))
+def test_polynomial_degree_2_call(prefix: str):
+    a0 = sc.scalar(-1.3, unit='K')
+    a1 = sc.scalar(0.03, unit='K/m')
+    a2 = sc.scalar(1.7, unit='K/m^2')
+    params = {f'{prefix}a0': a0, f'{prefix}a1': a1, f'{prefix}a2': a2}
+    m = model.PolynomialModel(degree=2, prefix=prefix)
+
+    x = sc.linspace('xx', 100.0, 234.0, 100, unit='m')
+    expected = a0 + a1 * x + a2 * x**2
+    actual = m(x, **params)
+    sc.testing.assert_allclose(actual, expected)
+
+
+@pytest.mark.parametrize('prefix', ('', 'lorentz_'))
+def test_gaussian_call(prefix: str):
+    amplitude = sc.scalar(2.8, unit='kg')
+    loc = sc.scalar(0.4, unit='m')
+    scale = sc.scalar(0.1, unit='m')
+    params = {
+        f'{prefix}amplitude': amplitude,
+        f'{prefix}loc': loc,
+        f'{prefix}scale': scale,
+    }
+    m = model.GaussianModel(prefix=prefix)
+
+    x = sc.linspace('xx', -2.0, 3.5, 200, unit='m')
+    expected = (
+        amplitude
+        / (math.sqrt(2 * math.pi) * scale)
+        * sc.exp(-((x - loc) ** 2) / (2 * scale**2))
+    )
+    actual = m(x, **params)
+    sc.testing.assert_allclose(actual, expected)
+
+
+@pytest.mark.parametrize('prefix', ('', 'lorentz_'))
+def test_lorentzian_call(prefix: str):
+    amplitude = sc.scalar(2.8, unit='kg')
+    loc = sc.scalar(0.4, unit='m')
+    scale = sc.scalar(0.1, unit='m')
+    params = {
+        f'{prefix}amplitude': amplitude,
+        f'{prefix}loc': loc,
+        f'{prefix}scale': scale,
+    }
+
+    m = model.LorentzianModel(prefix=prefix)
+
+    x = sc.linspace('xx', -4.0, 5.0, 200, unit='m')
+    expected = amplitude / np.pi * scale / ((x - loc) ** 2 + scale**2)
+    actual = m(x, **params)
+    sc.testing.assert_allclose(actual, expected)
+
+
+def test_polynomial_degree_1_guess_params():
+    a0 = sc.scalar(0.2, unit='cm')
+    a1 = sc.scalar(-5.1, unit='cm/s')
 
     x = sc.linspace('xx', 0.8, 4.3, 100, unit='s')
-    y = offset + slope * x
+    y = a0 + a1 * x
     data = sc.DataArray(y, coords={'xx': x})
 
-    m = model.LinearModel(prefix='linear_')
+    m = model.PolynomialModel(degree=1, prefix='')
     params = m.guess(data)
-    assert params.keys() == {'linear_slope', 'linear_offset'}
+    assert params.keys() == {'a0', 'a1'}
+    sc.testing.assert_allclose(params['a0'], a0, atol=sc.scalar(0.1, unit='cm'))
+    sc.testing.assert_allclose(params['a1'], a1, atol=sc.scalar(0.1, unit='cm/s'))
+
+
+def test_polynomial_degree_1_guess_params_prefix():
+    a0 = sc.scalar(0.2, unit='cm')
+    a1 = sc.scalar(-5.1, unit='cm/s')
+
+    x = sc.linspace('xx', 0.8, 4.3, 100, unit='s')
+    y = a0 + a1 * x
+    data = sc.DataArray(y, coords={'xx': x})
+
+    m = model.PolynomialModel(degree=1, prefix='linear_')
+    params = m.guess(data)
+    assert params.keys() == {'linear_a1', 'linear_a0'}
+    sc.testing.assert_allclose(params['linear_a0'], a0, atol=sc.scalar(0.1, unit='cm'))
     sc.testing.assert_allclose(
-        params['linear_slope'], slope, atol=sc.scalar(0.1, unit='cm/s')
+        params['linear_a1'], a1, atol=sc.scalar(0.1, unit='cm/s')
     )
-    sc.testing.assert_allclose(
-        params['linear_offset'], offset, atol=sc.scalar(0.1, unit='cm')
-    )
+
+
+def test_polynomial_degree_2_guess_params():
+    a0 = sc.scalar(-1.3, unit='K')
+    a1 = sc.scalar(0.03, unit='K/m')
+    a2 = sc.scalar(1.7, unit='K/m^2')
+
+    x = sc.linspace('xx', 100.0, 234.0, 100, unit='m')
+    y = a0 + a1 * x + a2 * x**2
+    data = sc.DataArray(y, coords={'xx': x})
+
+    m = model.PolynomialModel(degree=2, prefix='')
+    params = m.guess(data)
+    assert params.keys() == {'a0', 'a1', 'a2'}
+    sc.testing.assert_allclose(params['a0'], a0, atol=sc.scalar(0.1, unit='K'))
+    sc.testing.assert_allclose(params['a1'], a1, atol=sc.scalar(0.1, unit='K/m'))
+    sc.testing.assert_allclose(params['a2'], a2, atol=sc.scalar(0.1, unit='K/m^2'))
 
 
 def test_gaussian_guess_params_linspace():
@@ -134,6 +218,50 @@ def test_gaussian_guess_params_small_array():
     assert params.keys() == {'amplitude', 'loc', 'scale'}
     sc.testing.assert_allclose(
         params['amplitude'], amplitude, atol=sc.scalar(5.0, unit='kg')
+    )
+    sc.testing.assert_allclose(params['loc'], loc, atol=sc.scalar(0.5, unit='m'))
+    sc.testing.assert_allclose(params['scale'], scale, atol=sc.scalar(0.5, unit='m'))
+
+
+def test_gaussian_guess_params_prefix():
+    amplitude = sc.scalar(2.8, unit='kg')
+    loc = sc.scalar(0.4, unit='m')
+    scale = sc.scalar(0.1, unit='m')
+
+    x = sc.linspace('xx', -4.0, 5.0, 200, unit='m')
+    y = (
+        amplitude
+        / (math.sqrt(2 * math.pi) * scale)
+        * sc.exp(-((x - loc) ** 2) / (2 * scale**2))
+    )
+    data = sc.DataArray(y, coords={'xx': x})
+
+    m = model.GaussianModel(prefix='normal-')
+    params = m.guess(data)
+    assert params.keys() == {'normal-amplitude', 'normal-loc', 'normal-scale'}
+    sc.testing.assert_allclose(
+        params['normal-amplitude'], amplitude, atol=sc.scalar(0.5, unit='kg')
+    )
+    sc.testing.assert_allclose(params['normal-loc'], loc, atol=sc.scalar(0.5, unit='m'))
+    sc.testing.assert_allclose(
+        params['normal-scale'], scale, atol=sc.scalar(0.5, unit='m')
+    )
+
+
+def test_lorentzian_guess_params_linspace():
+    amplitude = sc.scalar(2.8, unit='kg')
+    loc = sc.scalar(0.4, unit='m')
+    scale = sc.scalar(0.1, unit='m')
+
+    x = sc.linspace('xx', -4.0, 5.0, 200, unit='m')
+    y = amplitude / np.pi * scale / ((x - loc) ** 2 + scale**2)
+    data = sc.DataArray(y, coords={'xx': x})
+
+    m = model.LorentzianModel(prefix='')
+    params = m.guess(data)
+    assert params.keys() == {'amplitude', 'loc', 'scale'}
+    sc.testing.assert_allclose(
+        params['amplitude'], amplitude, atol=sc.scalar(0.6, unit='kg')
     )
     sc.testing.assert_allclose(params['loc'], loc, atol=sc.scalar(0.5, unit='m'))
     sc.testing.assert_allclose(params['scale'], scale, atol=sc.scalar(0.5, unit='m'))
