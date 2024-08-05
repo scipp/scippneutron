131859ac43b149889fc409de1773fa9456a2e5b2
Johannes Kasimir <johanneskasimir@gmail.com>
2024-08-05 10:01:41 +0200

    test: more cylinder intersection tests

--- src/scippneutron/absorption/cylinder.py
+++ src/scippneutron/absorption/cylinder.py
@@ -3,6 +3,7 @@ from typing import Literal
 
 import numpy as np
 import scipp as sc
+from numpy.polynomial.chebyshev import chebgauss
 from numpy.polynomial.legendre import leggauss
 
 from . import quadratures
@@ -42,13 +43,16 @@ class Cylinder:
 
     def _select_quadrature_points(self, kind):
         if kind == 'expensive':
-            x, w = leggauss(10)
+            # x, w = leggauss(40)
+            x, w = chebgauss(35)
+            w *= (1 - x**2) ** 0.5
+            w /= sum(w) / 2
             quad = _cylinder_quadrature_from_product(
                 quadratures.disk254_cheb,
                 dict(x=x, weights=w),  # noqa: C408
             )
         elif kind == 'medium':
-            x, w = leggauss(8)
+            x, w = leggauss(40)
             # Would be nice to have a medium size Chebychev quadrature on the disk,
             # but I only found the large one for now.
             quad = _cylinder_quadrature_from_product(
@@ -61,6 +65,16 @@ class Cylinder:
                 quadratures.disk12,
                 dict(x=x, weights=w),  # noqa: C408
             )
+        elif kind == 'mc':
+            r = np.random.random(5000) ** 0.5
+            th = 2 * np.pi * np.random.random(5000)
+            z = 2 * np.random.random(5000) - 1
+            quad = {
+                'x': r * np.cos(th),
+                'y': r * np.sin(th),
+                'z': z,
+                'weights': 2 * np.pi * np.ones(5000) / 5000,
+            }
         else:
             raise NotImplementedError
         return {k: sc.array(dims=['quad'], values=v) for k, v in quad.items()}
@@ -151,19 +165,24 @@ def _line_infinite_cylinder_intersection(a, b, r, n):
     '''
     nxa = sc.cross(n, a)
     nxa_square = sc.dot(nxa, nxa)
+    parallel_to_cylinder = nxa_square == sc.scalar(0.0, unit=nxa.unit)
     s2 = nxa_square * r**2 - sc.dot(b, nxa) ** 2
     s = sc.sqrt(s2)
     m = sc.dot(nxa, sc.cross(b, a))
     intersection = s2 >= sc.scalar(0.0, unit=s2.unit)
-    left = (m - s) / nxa_square
-    right = (m + s) / nxa_square
+    left = sc.where(
+        parallel_to_cylinder,
+        sc.scalar(float('-inf'), unit=m.unit),
+        (m - s) / nxa_square,
+    )
+    right = sc.where(
+        parallel_to_cylinder, sc.scalar(float('inf'), unit=m.unit), (m + s) / nxa_square
+    )
     origin_in_cylinder = sc.norm(b - sc.dot(b, a) * a) <= r
-    ndota = sc.dot(n, a)
-    parallel_to_cylinder = sc.abs(ndota) == sc.scalar(1.0, unit=ndota.unit)
     return (
         sc.where(parallel_to_cylinder, origin_in_cylinder, intersection),
-        sc.where(parallel_to_cylinder, sc.scalar(float('-inf'), unit=left.unit), left),
-        sc.where(parallel_to_cylinder, sc.scalar(float('inf'), unit=right.unit), right),
+        left,
+        right,
     )
 
 
