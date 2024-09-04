a64cae0861118dc906512b12b72c95483cc3b5e7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-09-04 15:13:09 +0200

    Add missing import

--- src/scippneutron/meta/__init__.py
+++ src/scippneutron/meta/__init__.py
@@ -1,6 +1,6 @@
 """Metadata utilities."""
 
-from ._model import Beamline, Person
+from ._model import Beamline, Person, Software
 from ._orcid import ORCIDiD
 
-__all__ = ['Beamline', 'Person', 'ORCIDiD']
+__all__ = ['Beamline', 'Person', 'ORCIDiD', 'Software']
