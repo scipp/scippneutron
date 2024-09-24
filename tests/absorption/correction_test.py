ce57d0aa75f590a1e3b41029e8945ab1fae01138
Johannes Kasimir <johanneskasimir@gmail.com>
2024-09-24 13:22:40 +0200

    test: transmission correction

---
+++ tests/absorption/correction_test.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pytest
+import scipp as sc
+from scipp.testing import assert_allclose
+from scipy.integrate import quad
+
+from scippneutron.absorption import Cylinder, Material, compute_transmission_map
+
+
+@pytest.mark.parametrize('scattering_cross_section', [0.1, 0.5, 1.0])
+def test_compute_transmission_map(scattering_cross_section):
+    material = Material(
+        sc.scalar(0.0, unit='mm**2'),
+        sc.scalar(scattering_cross_section, unit='mm**2'),
+        sc.scalar(1.0, unit='1/mm**3'),
+    )
+    cylinder = Cylinder(
+        sc.vector([0, 1.0, 0]),
+        sc.vector([0, 0, 0.0], unit='mm'),
+        sc.scalar(1.0, unit='mm'),
+        sc.scalar(1.0, unit='mm'),
+    )
+
+    def transmission_fraction(effective_attenuation_factor):
+        return (
+            2
+            / np.pi
+            * quad(
+                lambda x: (
+                    np.sqrt(1 - x**2)
+                    * np.exp(-2 * effective_attenuation_factor * np.sqrt(1 - x**2))
+                ),
+                -1,
+                1,
+            )[0]
+        )
+
+    tm = compute_transmission_map(
+        cylinder,
+        material,
+        beam_direction=sc.vector([0, 0, 1]),
+        wavelength=sc.linspace('wavelength', 1.0, 1, 1, unit='angstrom'),
+        detector_position=sc.vectors(dims='x', values=[[0, 0, 1]], unit='m'),
+        quadrature_kind='expensive',
+    )
+    assert_allclose(
+        tm['wavelength', 0]['x', 0].data,
+        sc.scalar(transmission_fraction(scattering_cross_section)),
+        rtol=sc.scalar(1e-3),
+    )
+
+
+@pytest.mark.parametrize('scattering_cross_section', [0.1, 0.5, 1.0])
+def test_compute_transmission_map_wavelength_dependent(scattering_cross_section):
+    material = Material(
+        sc.scalar(scattering_cross_section / 5, unit='mm**2'),
+        sc.scalar(0, unit='mm**2'),
+        sc.scalar(1.0, unit='1/mm**3'),
+    )
+    cylinder = Cylinder(
+        sc.vector([0, 1.0, 0]),
+        sc.vector([0, 0, 0.0], unit='mm'),
+        sc.scalar(1.0, unit='mm'),
+        sc.scalar(1.0, unit='mm'),
+    )
+
+    def transmission_fraction(effective_attenuation_factor):
+        return (
+            2
+            / np.pi
+            * quad(
+                lambda x: (
+                    np.sqrt(1 - x**2)
+                    * np.exp(-2 * effective_attenuation_factor * np.sqrt(1 - x**2))
+                ),
+                -1,
+                1,
+            )[0]
+        )
+
+    tm = compute_transmission_map(
+        cylinder,
+        material,
+        beam_direction=sc.vector([0, 0, 1]),
+        wavelength=sc.linspace(
+            'wavelength', 5 * 1.7982, 5 * 1.7982, 1, unit='angstrom'
+        ),
+        detector_position=sc.vectors(dims='x', values=[[0, 0, 1]], unit='m'),
+        quadrature_kind='expensive',
+    )
+    assert_allclose(
+        tm['wavelength', 0]['x', 0].data,
+        sc.scalar(transmission_fraction(scattering_cross_section)),
+        rtol=sc.scalar(1e-3),
+    )
