06bbabb40682cdaae5ce356780ba64e3952a7181
Johannes Kasimir <johanneskasimir@gmail.com>
2024-10-09 15:19:08 +0200

    fix: use the atoms module to get scattering params

--- src/scippneutron/absorption/material.py
+++ src/scippneutron/absorption/material.py
@@ -2,24 +2,22 @@ from dataclasses import dataclass
 
 import scipp as sc
 
+from scippneutron.atoms import ScatteringParams, reference_wavelength
+
 
 @dataclass
 class Material:
-    # attenuation cross section at :math:`\lambda = 1.7982 \mathrm{angstrom}`.
-    attenuation_cross_section: sc.Variable
-    scattering_cross_section: sc.Variable
+    scattering_params: ScatteringParams
     effective_sample_number_density: sc.Variable
+    '''Density of the sample in number of formulas per cubic angstrom'''
 
     def attenuation_coefficient(self, wavelength: sc.Variable) -> sc.Variable:
         '''Computes marginal attenuation per distance for
         the given neutron wavelength.'''
         return self.effective_sample_number_density * (
-            self.scattering_cross_section
+            self.scattering_params.total_scattering_cross_section
             + (
-                self.attenuation_cross_section
-                * (
-                    wavelength
-                    / sc.scalar(1.7982, unit='angstrom').to(unit=wavelength.unit)
-                )
-            ).to(unit=self.scattering_cross_section.unit)
+                self.scattering_params.absorption_cross_section
+                * (wavelength / reference_wavelength().to(unit=wavelength.unit))
+            ).to(unit=self.scattering_params.total_scattering_cross_section.unit)
         )
