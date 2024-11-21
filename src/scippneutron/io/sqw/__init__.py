7ad411eea3f83d9db0727f67569f4c2631ef2f2f
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-11-21 16:27:10 +0100

    Start documenting SQW file format

--- src/scippneutron/io/sqw/__init__.py
+++ src/scippneutron/io/sqw/__init__.py
@@ -1,8 +1,42 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 # ruff: noqa: E402, F401
+
+"""SQW fie writer and reader.
+
+This module provides functionality for writing SQW files for
+`Horace <https://pace-neutrons.github.io/Horace>`_ and, to a limited extend,
+reading SQW files.
+The main entrypoint is :class:`Sqw`.
+
+.. rubric:: Classes
+
+.. autosummary::
+  :toctree: ../classes
+  :template: class-template.rst
+
+  Byteorder
+  EnergyMode
+  Sqw
+  SqwBuilder
+  SqwFileType
+
+.. rubric:: Models
+
+.. autosummary::
+  :toctree: ../classes
+  :template: class-template.rst
+
+  SqwFileHeader
+  SqwMainHeader
+  SqwIXExperiment
+  SqwDndMetadata
+  SqwLineAxes
+  SqwLineProj
+  SqwIXNullInstrument
+  SqwIXSample
+  SqwIXSource
+"""
 
 import importlib.metadata
 
@@ -14,7 +48,6 @@ except importlib.metadata.PackageNotFoundError:
 del importlib
 
 from ._sqw import Sqw
-
 from ._build import SqwBuilder
 from ._bytes import Byteorder
 from ._models import (
