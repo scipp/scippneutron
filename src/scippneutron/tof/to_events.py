c3823a6e444fd563c6aba7e16bcf7f69ea89e9d0
Neil Vaytet <neil.vaytet@ess.eu>
2024-12-11 10:43:36 +0100

    add support for masks in to_events

--- src/scippneutron/tof/to_events.py
+++ src/scippneutron/tof/to_events.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Scipp contributors (https://github.com/scipp)
 
+from functools import reduce
+
 import numpy as np
 import scipp as sc
 
@@ -13,6 +15,9 @@ def to_events(
     Each dimension with a bin-edge coordinate is converted to an event coordinate.
     The contract is that if we re-histogram the event list with the same bin edges,
     we should get the original counts back.
+    Masks on non-bin-edge dimensions are preserved.
+    If there are masks on bin-edge dimensions, the masked values are zeroed out in the
+    original data before the conversion to events.
 
     Parameters
     ----------
@@ -23,6 +28,8 @@ def to_events(
     events_per_bin:
         Number of events to generate per bin.
     """
+    if da.bins is not None:
+        raise ValueError("Cannot convert a binned DataArray to events.")
     rng = np.random.default_rng()
     event_coords = {}
     edge_dims = []
@@ -56,21 +63,37 @@ def to_events(
             dims=[event_dim, *edge_dims], values=events, unit=coord.unit
         )
 
+    # Find and apply masks that are on a bin-edge dimension
+    event_masks = {}
+    other_masks = {}
+    edge_dims_set = set(edge_dims)
+    for key, mask in da.masks.items():
+        if set(mask.dims) & edge_dims_set:
+            event_masks[key] = mask
+        else:
+            other_masks[key] = mask
+
+    data = da.data
+    if event_masks:
+        inv_mask = (~reduce(lambda a, b: a | b, event_masks.values())).to(dtype=int)
+        inv_mask.unit = ''
+        data = data * inv_mask
+
     # Create the data counts, which are the original counts divided by the number of
     # events per bin
     sizes = {event_dim: events_per_bin} | da.sizes
-    val = sc.broadcast(sc.values(da.data) / float(events_per_bin), sizes=sizes)
-    kwargs = {'dims': sizes.keys(), 'values': val.values, 'unit': da.data.unit}
-    if da.data.variances is not None:
+    val = sc.broadcast(sc.values(data) / float(events_per_bin), sizes=sizes)
+    kwargs = {'dims': sizes.keys(), 'values': val.values, 'unit': data.unit}
+    if data.variances is not None:
         kwargs['variances'] = sc.broadcast(
-            sc.variances(da.data) / float(events_per_bin), sizes=sizes
+            sc.variances(data) / float(events_per_bin), sizes=sizes
         ).values
-    data = sc.array(**kwargs)
+    new_data = sc.array(**kwargs)
 
-    new = sc.DataArray(data=data, coords=event_coords)
+    new = sc.DataArray(data=new_data, coords=event_coords)
     new = new.transpose((*midp_dims, *edge_dims, event_dim)).flatten(
         dims=[*edge_dims, event_dim], to=event_dim
     )
-    for dim in midp_dims:
-        new.coords[dim] = da.coords[dim].copy()
-    return new
+    return new.assign_coords(
+        {dim: da.coords[dim].copy() for dim in midp_dims}
+    ).assign_masks({key: mask.copy() for key, mask in other_masks.items()})
