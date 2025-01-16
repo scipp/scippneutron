20fd77bc2ed370cb5e62270ee521c3e8c1d3764f
Neil Vaytet <neil.vaytet@ess.eu>
2025-01-16 08:26:34 +0100

    change name to PivotTimeAtDetector

--- src/scippneutron/tof/unwrap.py
+++ src/scippneutron/tof/unwrap.py
@@ -91,9 +91,9 @@ UnwrappedTimeOfArrival = NewType('UnwrappedTimeOfArrival', sc.Variable)
 Time of arrival of the neutron at the detector, unwrapped at the pulse period.
 """
 
-FrameAtDetectorStartTime = NewType('FrameAtDetectorStartTime', sc.Variable)
+PivotTimeAtDetector = NewType('PivotTimeAtDetector', sc.Variable)
 """
-Time of the start of the frame at the detector.
+Pivot time at the detector, i.e., the time of the start of the frame at the detector.
 """
 
 UnwrappedTimeOfArrivalMinusStartTime = NewType(
@@ -302,11 +302,12 @@ def masked_tof_lookup_table(
     return MaskedTimeOfFlightLookupTable(out)
 
 
-def frame_at_detector_start_time(
+def pivot_time_at_detector(
     simulation: SimulationResults, ltotal: Ltotal
-) -> FrameAtDetectorStartTime:
+) -> PivotTimeAtDetector:
     """
-    Compute the start time of the frame at the detector.
+    Compute the pivot time at the detector, i.e., the time of the start of the frame at
+    the detector.
     The assumption here is that the fastest neutron in the simulation results is the one
     that arrives at the detector first.
     One could have an edge case where a slightly slower neutron which is born earlier
@@ -327,7 +328,7 @@ def frame_at_detector_start_time(
     toa = time_at_simulation + (dist / simulation.speed[ind]).to(
         unit=time_at_simulation.unit, copy=False
     )
-    return FrameAtDetectorStartTime(toa)
+    return PivotTimeAtDetector(toa)
 
 
 def unwrapped_time_of_arrival(
@@ -369,7 +370,7 @@ def unwrapped_time_of_arrival(
 
 
 def unwrapped_time_of_arrival_minus_frame_start_time(
-    toa: UnwrappedTimeOfArrival, start_time: FrameAtDetectorStartTime
+    toa: UnwrappedTimeOfArrival, pivot_time: PivotTimeAtDetector
 ) -> UnwrappedTimeOfArrivalMinusStartTime:
     """
     Compute the time of arrival of the neutron at the detector, unwrapped at the pulse
@@ -381,12 +382,13 @@ def unwrapped_time_of_arrival_minus_frame_start_time(
     ----------
     toa:
         Time of arrival of the neutron at the detector, unwrapped at the pulse period.
-    start_time:
-        Time of the start of the frame at the detector.
+    pivot_time:
+        Pivot time at the detector, i.e., the time of the start of the frame at the
+        detector.
     """
     # Order of operation to preserve dimension order
     return UnwrappedTimeOfArrivalMinusStartTime(
-        -start_time.to(unit=elem_unit(toa), copy=False) + toa
+        -pivot_time.to(unit=elem_unit(toa), copy=False) + toa
     )
 
 
@@ -414,7 +416,7 @@ def time_of_arrival_minus_start_time_modulo_period(
 
 def time_of_arrival_folded_by_frame(
     toa: TimeOfArrivalMinusStartTimeModuloPeriod,
-    start_time: FrameAtDetectorStartTime,
+    pivot_time: PivotTimeAtDetector,
 ) -> FrameFoldedTimeOfArrival:
     """
     The time of arrival of the neutron at the detector, folded by the frame period.
@@ -424,11 +426,12 @@ def time_of_arrival_folded_by_frame(
     toa:
         Time of arrival of the neutron at the detector, unwrapped at the pulse period,
         minus the start time of the frame, modulo the frame period.
-    start_time:
-        Time of the start of the frame at the detector.
+    pivot_time:
+        Pivot time at the detector, i.e., the time of the start of the frame at the
+        detector.
     """
     return FrameFoldedTimeOfArrival(
-        toa + start_time.to(unit=elem_unit(toa), copy=False)
+        toa + pivot_time.to(unit=elem_unit(toa), copy=False)
     )
 
 
@@ -519,17 +522,17 @@ def providers():
     Providers of the time-of-flight workflow.
     """
     return (
-        pulse_period_from_source,
+        compute_tof_lookup_table,
         frame_period,
+        masked_tof_lookup_table,
+        pivot_time_at_detector,
+        pulse_period_from_source,
         run_tof_model,
-        frame_at_detector_start_time,
+        time_of_arrival_folded_by_frame,
+        time_of_arrival_minus_start_time_modulo_period,
+        time_of_flight_data,
         unwrapped_time_of_arrival,
         unwrapped_time_of_arrival_minus_frame_start_time,
-        time_of_arrival_minus_start_time_modulo_period,
-        time_of_arrival_folded_by_frame,
-        time_of_flight_data,
-        compute_tof_lookup_table,
-        masked_tof_lookup_table,
     )
 
 
