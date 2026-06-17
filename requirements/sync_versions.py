592352c0dd46c4771a0dc40fda2246cacd265b3f
Neil Vaytet <neil.vaytet@ess.eu>
2026-06-17 13:52:37 +0200

    lint

--- requirements/sync_versions.py
+++ requirements/sync_versions.py
@@ -1,7 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import argparse
+import logging
 from pathlib import Path
+
+logger = logging.getLogger(" ")
+logging.basicConfig(level=logging.INFO)
 
 
 def main(fname, requirements_folder):
@@ -9,9 +13,9 @@ def main(fname, requirements_folder):
     Get versions from requirements folder and copy them to the conda env file.
     This ensures that the same versions are used across both pip and conda environments.
     """
-    print(f"Syncing versions from {requirements_folder} to {fname}\n")
+    logger.info("Syncing versions from %s to %s\n", requirements_folder, fname)
 
-    with open(fname, "r") as f:
+    with open(fname) as f:
         lines = f.readlines()
 
     with open(fname, "w") as f:
@@ -29,12 +33,14 @@ def main(fname, requirements_folder):
             for req_file in requirements_folder.glob("*.txt"):
                 if "nightly" in req_file.name:
                     continue
-                with open(req_file, "r") as req_f:
+                with open(req_file) as req_f:
                     for req_line in req_f:
                         if pattern in req_line:
                             version = req_line.split("==")[1].strip()
                             if version != old_version:
-                                print(f"{package_name}: {old_version} --> {version}")
+                                logger.info(
+                                    "%s: %s --> %s", package_name, old_version, version
+                                )
                             f.write(f"  {package_name}=={version}\n")
                             found = True
                             break
@@ -47,7 +53,7 @@ def main(fname, requirements_folder):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Get versions from requirements folder and copy them to the conda env file."
+        description="Synchronize versions from requirements folder to conda env file."
     )
     parser.add_argument("fname", type=str, help="Path to conda env file.")
     parser.add_argument(
